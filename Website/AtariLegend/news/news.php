1d17b54e50a666ca13aa11c18fc0a8737c6b3b0c
stsilversurfer <silversurfer@atari-forum.com>
2015-04-14 20:45:31 +0200

    Update news.php
    Clean up of news query and code

--- Website/AtariLegend/news/news.php
+++ Website/AtariLegend/news/news.php
@@ -18,20 +18,12 @@
 //load all common functions
 include("../includes/common.php"); 
 
-$v_counter = (isset($_GET["v_counter"]) ? $_GET["v_counter"] : 0);
-
 //Select the news from the DB
 $query_news = mysql_query("SELECT * FROM news 
 						   LEFT JOIN news_image ON (news.news_image_id = news_image.news_image_id) 
 						   LEFT JOIN users ON (news.user_id = users.user_id) 
-						   ORDER BY news.news_date DESC LIMIT  " . $v_counter . ", 6"); 
+						   ORDER BY news.news_date DESC LIMIT 6"); 
 						 
-
-//check the number of news updates
-$query_number = mysql_query("SELECT count(*) FROM news ORDER BY news_date DESC") or die("Couldn't get the number of news stories");
-$v_rows = mysql_result($query_number,0,0) or die("Couldn't get the number of news stories");
-
-
 //Lets put all the acquired news data into a smarty array and send them to the template.
 while ($sql_news = mysql_fetch_array($query_news))  
 {	
@@ -53,9 +45,9 @@ while ($sql_news = mysql_fetch_array($query_news))
 	
 	$smarty->append('news',
 	    array('news_date' => $news_date,
-			  'news_headline' => $sql_news['news_headline'],
+		  'news_headline' => $sql_news['news_headline'],
         	  'news_text' => $news_text,
-		      'image' => $v_image));
+		  'image' => $v_image));
 }
 
 //Lets get some trivia quotes
