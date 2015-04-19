3f27b04ebf6c97f9dd47c245b80d58e1dfd5e9b8
@stsilversurfer <silversurfer@atari-forum.com>
2015-04-19 12:54:54 +0200

    add statistics php

--- Website/AtariLegend/front/front.php
+++ Website/AtariLegend/front/front.php
@@ -23,6 +23,7 @@ include("../tiles/did_you_know_tile.php");
 include("../tiles/latest_reviews_tile.php"); 
 include("../tiles/who_is_it_tile.php"); 
 include("../tiles/screenstar.php");
+include("../tiles/statistics_tile.php");
 
 //Send all smarty variables to the templates
 $smarty->display('extends:../templates/0/main.html|../templates/0/frontpage.html|../templates/0/latest_news_tile.html|../templates/0/latest_reviews_tile.html|../templates/0/who_is_it_tile.html|../templates/0/screenstar_tile.html|../templates/0/hotlinks_tile.html|../templates/0/date_quote_tile.html|../templates/0/did_you_know_tile.html|../templates/0/statistics_tile.html');
