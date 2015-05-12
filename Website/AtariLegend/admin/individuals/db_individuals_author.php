e72177134cd5bd9b09753d5cf371fbe6a6ed3791
@stsilversurfer <silversurfer@atari-forum.com>
2015-05-12 22:08:37 +0200

    cpanel, bug fixing on individuals section.

--- Website/AtariLegend/admin/individuals/db_individuals_author.php
+++ Website/AtariLegend/admin/individuals/db_individuals_author.php
@@ -1,17 +1,16 @@
 <?
 /***************************************************************************
-*                                individuals_auhtor.php
+*                                db_individuals_auhtor.php
 *                            -----------------------------
 *   begin                : Saturday, August 6, 2005
 *   copyright            : (C) 2005 Atari Legend
-*   email                : maarten.martens@freebel.net
 *   actual update        : Creation of file
-*   Id: individuals_auhtor.php,v 0.10 2005/08/06 19:07 Gatekeeper
+*
 *
 ***************************************************************************/
 
 //****************************************************************************************
-// The Author main page
+// The db Author main page
 //**************************************************************************************** 
 
 include("../includes/common.php");
@@ -22,6 +21,8 @@ if ( isset($action) and $action == 'insert' )
 				  or die("Couldn't insert into author_type");
 	
 	$smarty->assign("message",'Insert succesfull');
+
+header("Location: ../individuals/individuals_author.php");
 }
 
 if ( isset($action) and $action == 'edit' )
@@ -30,6 +31,8 @@ if ( isset($action) and $action == 'edit' )
 				  or die("Couldn't edit the author type");
 	
 	$smarty->assign("message",'Update succesfull');
+
+header("Location: ../individuals/individuals_author.php");
 }
 
 if ( isset($action) and $action == 'delete' )
@@ -38,40 +41,10 @@ if ( isset($action) and $action == 'delete' )
 				  or die("Couldn't delete from author_type");
 	
 	$smarty->assign("message",'Delete succesfull');
+
+header("Location: ../individuals/individuals_author.php");
 }
-
-if ( isset($action) and $action == 'load' )
-{
-		
-	$sql_author = mysql_query("SELECT * FROM author_type WHERE author_type_id = '$type_id' 
-							   ORDER BY author_type_info ASC")
-			      or die ("Couldn't query author_types");
-			  
-	while  ($author=mysql_fetch_array($sql_author)) 
-	{  
-		$smarty->assign('author_types_load',
-	 		 array('author_type' => $author['author_type_info'],
-				   'author_type_id' => $author['author_type_id']));
-	
-		$smarty->assign("load", '1');
-	}
-}
-
-$sql_author = mysql_query("SELECT * FROM author_type ORDER BY author_type_info ASC")
-		      or die ("Couldn't query author_types");
-		
-while  ($author=mysql_fetch_array($sql_author)) 
-{  
-	$smarty->append('author_types',
- 		 array('author_type' => $author['author_type_info'],
-			   'author_type_id' => $author['author_type_id']));
-}
-
-$smarty->assign("user_id",$_SESSION['user_id']);
-$smarty->assign('individuals_author_tpl', '1');
-
-//Send all smarty variables to the templates
-$smarty->display('file:../templates/0/index.tpl');
 
 //close the connection
 mysql_close();
+?>
