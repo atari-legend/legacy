66fa090b7a9b9ae71c39e1a0e6cada651771834c
@stsilversurfer <silversurfer@atari-forum.com>
2015-05-16 10:53:46 +0200

    cpanel, news smarty and php error fixes.

--- Website/AtariLegend/admin/news/news_edit.php
+++ Website/AtariLegend/admin/news/news_edit.php
@@ -66,6 +66,25 @@ $sql_news  =  mysql_query("SELECT
 			  			   user_id,
 			  			   news_date
 			  			   FROM news_submission WHERE news_submission_id = '$news_submission_id'");
+
+$news = mysql_fetch_array($sql_news);
+
+$user_name = get_username_from_id($news['user_id']);
+$news_date = convert_timestamp($news['news_date']);
+//$news_text = InsertALCode($news['news_text']);
+//$news_text = InsertSmillies($news_text);
+		
+
+$smarty->assign('edit_submissions',
+   		array('edit_userid' => $user_name,
+			  'edit_submission_id' => $news_submission_id,
+			  'edit_headline' => $news['news_headline'],
+			  'edit_date' => $news_date,
+			  'edit_text' => $news['news_text'],
+			  'edit_image_id' => $news['news_image_id']));	
+
+
+
 }
 else
 {
@@ -77,7 +96,7 @@ $sql_news  =  mysql_query("SELECT
 			  			   user_id,
 			  			   news_date
 			  			   FROM news WHERE news_id = '$news_id'");
-}
+
 
 $news = mysql_fetch_array($sql_news);
 
@@ -88,13 +107,12 @@ $news_date = convert_timestamp($news['news_date']);
 		
 $smarty->assign('edit_submissions',
    		array('edit_userid' => $user_name,
-			  'edit_submission_id' => $news_submission_id,
 			  'edit_id' => $news_id,
 			  'edit_headline' => $news['news_headline'],
 			  'edit_date' => $news_date,
 			  'edit_text' => $news['news_text'],
 			  'edit_image_id' => $news['news_image_id']));	
-
+}
 
 //Get the news images
 $sql_newsimage = mysql_query("SELECT news_image_id,news_image_name FROM news_image ORDER BY news_image_name");
