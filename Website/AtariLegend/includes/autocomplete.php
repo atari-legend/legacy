52a6194dd6f18906da73019d7e345bf7f9631251
@stsilversurfer <silversurfer@atari-forum.com>
2015-05-24 01:32:34 +0200

    fronpage, autocomplete.php added support for game aka's, demos, demo aka's.

--- Website/AtariLegend/includes/autocomplete.php
+++ Website/AtariLegend/includes/autocomplete.php
@@ -5,7 +5,20 @@
 include("../includes/connect.php"); 
 $text = $mysqli->real_escape_string($_GET['term']);
 
-$query = "SELECT * FROM game WHERE game_name LIKE '%$text%' ORDER BY game_name ASC LIMIT 10";
+//Set up queries
+$game_name_query = "SELECT game_name FROM game";
+$game_aka_name_query = "SELECT aka_name AS game_name FROM aka_game";
+$demo_name_query = "SELECT demo_name AS game_name FROM demo";
+$demo_aka_name_query = "SELECT aka_name AS game_name FROM demo_aka";
+
+//Create a temporary table to get all the demos and aka's as well.
+$mysqli->query("CREATE TEMPORARY TABLE temp ENGINE=MEMORY $game_name_query");
+$mysqli->query("INSERT INTO temp $game_aka_name_query");
+$mysqli->query("INSERT INTO temp $demo_name_query");
+$mysqli->query("INSERT INTO temp $demo_aka_name_query");
+
+//Get the results
+$query = "SELECT game_name FROM temp WHERE game_name LIKE '%$text%' ORDER BY game_name ASC LIMIT 10";
 $result = $mysqli->query($query);
 $json = '[';
 $first = true;
@@ -22,5 +35,6 @@ while($row = $result->fetch_assoc())
     $json .= '{"value":"'.$row['game_name'].'"}';
 }
 $json .= ']';
+$mysqli->query("DROP TABLE temp");
 echo $json;
 ?>
