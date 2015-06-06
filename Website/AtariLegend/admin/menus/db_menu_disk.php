461f95296e114a3d34f8eb762b867ca1d145b746
@stsilversurfer <silversurfer@atari-forum.com>
2015-06-07 01:04:52 +0200

    cpanel, menus - added functionality to add a new menu disk.

--- Website/AtariLegend/admin/menus/db_menu_disk.php
+++ Website/AtariLegend/admin/menus/db_menu_disk.php
@@ -31,6 +31,55 @@ if(isset($menu_sets_name))
 header("Location: ../menus/menus_list.php");
 
 }
+
+//****************************************************************************************
+// Add new menu disk
+//**************************************************************************************** 
+
+if($action=="add_new_menu_disk")
+{
+	if ($menu_disk_number!=='' or $menu_disk_letter!=='')
+	{	
+		//first we start with if this is numbered menu disk
+		if($menu_disk_number!=='' and $menu_disk_letter=='')
+			{
+				$sql = $mysqli->query("INSERT INTO menu_disk (menu_sets_id,menu_disk_number) VALUES ('$menu_sets_id','$menu_disk_number')");
+				$last_id = $mysqli->insert_id;
+				if($menu_disk_part!=='') 
+					{
+					$sql = $mysqli->query("UPDATE menu_disk SET menu_disk_part='$menu_disk_part' 
+					    WHERE menu_disk_id='$last_id'");
+					}
+				if($menu_disk_version!=='') 
+					{
+					$sql = $mysqli->query("UPDATE menu_disk SET menu_disk_version='$menu_disk_version' 
+					    WHERE menu_disk_id='$last_id'");
+					}
+			
+			}
+		//Ok, but if it is not a numbered disk but instead it is one of those horrible alphabetic disks
+		if($menu_disk_number=='' and $menu_disk_letter!=='')
+			{
+				$sql = $mysqli->query("INSERT INTO menu_disk (menu_sets_id,menu_disk_letter) VALUES ('$menu_sets_id','$menu_disk_letter')");
+				$last_id2 = $mysqli->insert_id;
+				if($menu_disk_part!=='') 
+					{
+					$sql = $mysqli->query("UPDATE menu_disk SET menu_disk_part='$menu_disk_part' 
+					    WHERE menu_disk_id='$last_id2'");
+					}
+				if($menu_disk_version!=='') 
+					{
+					$sql = $mysqli->query("UPDATE menu_disk SET menu_disk_version='$menu_disk_version' 
+					    WHERE menu_disk_id='$last_id2'");
+					}
+			
+			}		
+	}
+
+header("Location: ../menus/menus_disk_list.php?menu_sets_id=$menu_sets_id");
+
+}
+
 //****************************************************************************************
 // Edit menu_set name
 //**************************************************************************************** 
