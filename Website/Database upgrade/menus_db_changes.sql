9660bc5cad39d3ba480ccea594d2cd5370c08dd5
@stsilversurfer <silversurfer@atari-forum.com>
2015-06-06 13:56:43 +0200

    database, small change for the database, some tiny ints converted to int.

---
+++ Website/Database upgrade/menus_db_changes.sql
@@ -0,0 +1,3 @@
+ALTER TABLE  `crew_menu_prod` CHANGE  `crew_menu_prod_id`  `crew_menu_prod_id` INT( 11 ) NULL DEFAULT NULL ,
+CHANGE  `crew_id`  `crew_id` INT( 11 ) NULL DEFAULT NULL ,
+CHANGE  `menu_sets_id`  `menu_sets_id` INT( 11 ) NULL DEFAULT NULL
