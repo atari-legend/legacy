d7c265d0d6289b3689ce221dc2121637f332d17c
@stsilversurfer <silversurfer@atari-forum.com>
2015-06-07 23:06:06 +0200

    wendy, stonish import script.

--- Website/AtariLegend/includes/config.php
+++ Website/AtariLegend/includes/config.php
@@ -11,18 +11,19 @@
 ***************************************************************************/
 
 // Set variables 
-$game_screenshot_path="../data/images/game_screenshots/";
 $article_screenshot_path="../data/images/article_screenshots/";
-$game_gallery_path="../data/images/game_gallery/"; 
+$company_screenshot_path="../data/images/company_logos/";
 $game_boxscan_path="../data/images/game_boxscans/";
 $game_file_path="../data/zips/games/";
 $game_file_temp_path="../data/zips/temp/";
-$company_screenshot_path="../data/images/company_logos/";
 $game_file_path="../data/zips/games/";
+$game_gallery_path="../data/images/game_gallery/"; 
+$game_screenshot_path="../data/images/game_screenshots/";
+$individual_screenshot_path="../data/images/individual_screenshots/";
+$interview_screenshot_path="../data/images/interview_screenshots/";
+$menu_screenshot_path="../data/images/menu_screenshots/";
+$music_game_path="../data/music/games/";
 $news_images_path="../data/images/news_images/";
 $website_image_path="../data/images/website_images/";
-$interview_screenshot_path="../data/images/interview_screenshots/";
-$individual_screenshot_path="../data/images/individual_screenshots/";
 $trivia_screenshot_path="../data/images/trivia_screens/";
-$music_game_path="../data/music/games/";
 ?>
