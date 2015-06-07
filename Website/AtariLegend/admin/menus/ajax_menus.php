564eebdcf72589b8a3a8c7605a2bb2720b96f5c3
@stsilversurfer <silversurfer@atari-forum.com>
2015-06-07 14:01:14 +0200

    cpanel, menus - close menu disk expand.

--- Website/AtariLegend/admin/menus/ajax_menus.php
+++ Website/AtariLegend/admin/menus/ajax_menus.php
@@ -123,6 +123,62 @@ include("../includes/constants.php");
 				$smarty->assign('menu_disk_id', $menu_disk_id);
 	}
 
+	// CLOSE EDIT BOX FOR A MENU DISK!			
+	if (isset($action) and $action=="close_edit_disk_box" and $menu_disk_id!=='')
+	{
+		
+		$sql_menus = "SELECT menu_disk.menu_sets_id,
+						menu_set.menu_sets_name,
+						menu_disk.menu_disk_id,
+						menu_disk.menu_disk_number,
+						menu_disk.menu_disk_letter,
+						menu_disk.menu_disk_version,
+						menu_disk.menu_disk_part,
+						crew.crew_id,
+						crew.crew_name,
+						menu_disk.state
+						FROM menu_disk 
+						LEFT JOIN menu_set ON (menu_disk.menu_sets_id = menu_set.menu_sets_id)
+						LEFT JOIN crew_menu_prod ON (menu_set.menu_sets_id = crew_menu_prod.menu_sets_id)
+						LEFT JOIN crew ON (crew_menu_prod.crew_id = crew.crew_id)
+						WHERE menu_disk.menu_disk_id = '$menu_disk_id'";
+				
+				$result_menus= $mysqli->query($sql_menus);
+				$row=$result_menus->fetch_array(MYSQLI_BOTH);
+				
+				// Create Menu disk name
+				$menu_disk_name = "$row[menu_sets_name] ";
+				if(isset($row['menu_disk_number'])) {$menu_disk_name .= "$row[menu_disk_number]";}
+				if(isset($row['menu_disk_letter'])) {$menu_disk_name .= "$row[menu_disk_letter]";}
+				if(isset($row['menu_disk_part'])) 
+					{
+						if (is_numeric($row['menu_disk_part']))
+							{$menu_disk_name .= " part $row[menu_disk_part]";}
+							else 
+							{
+								$menu_disk_name .= "$row[menu_disk_part]";
+							}
+					}
+				if(isset($row['menu_disk_version']) and $row['menu_disk_version']!=='') {$menu_disk_name .= " v$row[menu_disk_version]";}
+
+					$smarty->assign('menus',
+	   			 	 array('menu_sets_id' => $row['menu_sets_id'],
+						   'menu_sets_name' => $row['menu_sets_name'],
+						   'menu_disk_name' => $menu_disk_name,
+						   'menu_disk_id' => $row['menu_disk_id'],
+						   'menu_disk_number' => $row['menu_disk_number'],
+						   'menu_disk_letter' => $row['menu_disk_letter'],
+						   'menu_disk_version' => $row['menu_disk_version'],
+						   'menu_disk_part' => $row['menu_disk_part'],
+						   'crew_id' => $row['crew_id'],
+						   'crew_name' => $row['crew_name'],
+						   'state' => $row['state']));
+		
+		
+				$smarty->assign('smarty_action', 'close_edit_disk_box');
+				$smarty->assign('menu_disk_id', $menu_disk_id);
+	}
+
 //Send all smarty variables to the templates
 $smarty->display('file:../templates/0/ajax_menus.html');
 ?>
