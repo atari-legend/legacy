f87c01498549324b525a47d4f4bb773d2db00b36
stgraveyard <martens_maarten@hotmail.com>
2015-09-04 22:14:38 +0200

    Cpanel update
    Hello friends,
    
    In this update I have completed the first parts of the CPANEL. Every
    single file has been tested in each skin and on all resolutions
    (responsiveness). I've also tested in 2 browers, IE and FF.
    
    The sections that are now complete :
    - Main Page/Stats and all functions and links inside
    - Forum --> This is just a link to AF
    - Did you know
    - Trivia Quotes
    - User admin page and all functions and sections inside
    
    Cheers,
    Maarten

--- Website/AtariLegend/php/includes/config.php
+++ Website/AtariLegend/php/includes/config.php
@@ -59,4 +59,5 @@ $magazine_scan_path="../../data/images/magazine_scans/";
 
 //users
 $user_avatar_path="../../data/images/user_avatars/";
+$user_avatar_save_path="../../../data/images/user_avatars/";
 ?>
