3b0685f6132e942d8d5195fa653609e2409dfa1b
stgraveyard <martens_maarten@hotmail.com>
2015-09-10 22:46:20 +0200

    CPANEL - Link section
    Add/validate links : I've combined these 2 pages into 1 - I'm
    experiencing an error. Can anyone help?

--- Website/AtariLegend/php/admin/links/link_addnew.php
+++ Website/AtariLegend/php/admin/links/link_addnew.php
@@ -9,6 +9,7 @@
 *							
 *
 *   Id: link_addnew.php,v 0.10 2005/01/08 Silver Surfer
+*   Id: link_addnew.php,v 0.20 2015/09/07 ST Graveyard
 *
 ***************************************************************************/
 
@@ -18,20 +19,63 @@ In this section we can add a new link to the DB
 ***********************************************************************************
 */
 
-include("../includes/common.php"); 
+include("../../includes/common.php");
 
 
-	$RESULT=$mysqli->query("SELECT * FROM website_category ORDER BY website_category_name");
-	
-	while ($rowlinkcat = $RESULT->fetch_array(MYSQLI_BOTH)) 
-	{ 
-		   			$smarty->append('website_category',
-	    			array('website_category_id' => $rowlinkcat['website_category_id'],
-						  'website_category_name' => $rowlinkcat['website_category_name']));
-	} 
+$RESULT=$mysqli->query("SELECT * FROM website_category ORDER BY website_category_name");
+
+while ($rowlinkcat = $RESULT->fetch_array(MYSQLI_BOTH)) 
+{ 
+	$smarty->append('website_category',
+	array('website_category_id' => $rowlinkcat['website_category_id'],
+		  'website_category_name' => $rowlinkcat['website_category_name']));
+} 
 
 $smarty->assign("user_id",$_SESSION['user_id']);
 
+//****************************************************************************************
+// VALIDATE LINKS THAT HAVE SUBMITTED THROUGH THE FRONTPAGE
+//****************************************************************************************
+$sql = "SELECT * FROM website_validate
+		LEFT JOIN users ON (website_validate.user_id = users.user_id)";
+
+$result = $mysqli->query($sql) or die("couldn't query website_validate");
+
+if (get_rows($result)<1)
+	{
+		$section = "Validate links";
+		$error_msg = "No links has been submitted.";
+	
+			$smarty->assign('error_msg',
+	    			  array('section' => $section,
+			  	  			'message' => $error_msg));
+	
+		$smarty->assign('error_message_tpl', '1');
+	}
+else 
+	{ 
+	  while ($valrow = $result->fetch_array(MYSQLI_BOTH))
+ 		{
+		$link_sub = get_rows($mysqli->query("SELECT website_user_sub FROM website WHERE website_user_sub='$valrow[user_id]'"));
+		
+		$website_date = convert_timestamp($valrow['website_date']); 
+		$user_name = get_username_from_id($valrow['user_id']);
+				
+		$smarty->append('validate',
+			array('website_id' => $valrow['website_id'],
+				  'website_name' => $valrow['website_name'],
+				  'website_url' => $valrow['website_url'],
+				  'website_date' => $website_date,
+				  'website_category' => $valrow['website_category'],
+				  'website_description' => $valrow['website_description'],
+				  'user_email' => $valrow['email'],
+				  'link_sub' => $link_sub,
+				  'user_name' => $user_name));
+		} 
+	}
+
+$smarty->assign('left_nav', 'leftnav_position_linkaddnew');
+
 //Send all smarty variables to the templates
-$smarty->display('file:../templates/0/link_addnew.html');
+$smarty->display('extends:../../../templates/html/admin/main.html|../../../templates/html/admin/frontpage.html|../../../templates/html/admin/link_addnew.html|../../../templates/html/admin/left_nav.html');
 ?>
