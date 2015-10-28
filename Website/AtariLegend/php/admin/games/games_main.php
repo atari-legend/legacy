6ad81ee4b5780c4637d69bb3230c3801f8020320
stgraveyard <martens_maarten@hotmail.com>
2015-10-29 00:06:17 +0100

    games main page
    CPANEL - Games main page has been converted to 3 skins and is
    responsive. Not functional yet.

--- Website/AtariLegend/php/admin/games/games_main.php
+++ Website/AtariLegend/php/admin/games/games_main.php
@@ -7,6 +7,7 @@
 *   email                : admin@atarilegend.com
 *
 *   Id: games_main.php,v 0.10 2005/08/28 17:30 Gatekeeper
+*   Id: games_main.php,v 0.20 2015/08/24 22:24 Gatekeeper
 *
 ***************************************************************************/
 
@@ -17,64 +18,66 @@ This is the game browse page where you can navigate your way through the games d
 */
 
 //load all common functions
-include("../includes/common.php"); 
+include("../../includes/common.php"); 
 
 date_default_timezone_set('UTC');
 $start = microtime(true);
 
-		//Get publisher values to fill the searchfield
-		$sql_publisher = $mysqli->query("SELECT pub_dev.pub_dev_id,
-											 pub_dev.pub_dev_name
-											 FROM game_publisher
-											 LEFT JOIN pub_dev ON (game_publisher.pub_dev_id = pub_dev.pub_dev_id)
-											 GROUP BY pub_dev.pub_dev_id HAVING COUNT(DISTINCT pub_dev.pub_dev_id) = 1
-											 ORDER BY pub_dev.pub_dev_name ASC") 
-										or die("Problems retriving values from publishers.");
-		
-		while ($company_publisher = $sql_publisher->fetch_array(MYSQLI_BOTH))
-		{
-		
-			$smarty->append('company_publisher',
-	    		 array('comp_id' => $company_publisher['pub_dev_id'],
-				 	   'comp_name' => $company_publisher['pub_dev_name']));
-		
-		}
-		
-		//Get Developer values to fill the searchfield
-		$sql_developer = $mysqli->query("SELECT pub_dev.pub_dev_id,
-											 pub_dev.pub_dev_name
-											 FROM game_developer
-											 LEFT JOIN pub_dev ON (game_developer.dev_pub_id = pub_dev.pub_dev_id)
-											 GROUP BY pub_dev.pub_dev_id HAVING COUNT(DISTINCT pub_dev.pub_dev_id) = 1
-											 ORDER BY pub_dev.pub_dev_name ASC") 
-										or die("Problems retriving values from developers.");
-		
-		while ($company_developer = $sql_developer->fetch_array(MYSQLI_BOTH))
-		{
-		
-			$smarty->append('company_developer',
-	    		 array('comp_id' => $company_developer['pub_dev_id'],
-				 	   'comp_name' => $company_developer['pub_dev_name']));
-		
-		}
-		
-		//get the number of games in the archive
-		$query_number = $mysqli->query("SELECT * FROM game") or die("Couldn't get the number of games");
-		$v_rows = get_rows($query_number) or die("Couldn't get the number of games");
+//Get publisher values to fill the searchfield
+$sql_publisher = $mysqli->query("SELECT pub_dev.pub_dev_id,
+									 pub_dev.pub_dev_name
+									 FROM game_publisher
+									 LEFT JOIN pub_dev ON (game_publisher.pub_dev_id = pub_dev.pub_dev_id)
+									 GROUP BY pub_dev.pub_dev_id HAVING COUNT(DISTINCT pub_dev.pub_dev_id) = 1
+									 ORDER BY pub_dev.pub_dev_name ASC") 
+								or die("Problems retriving values from publishers.");
 
-		$smarty->assign('games_nr', $v_rows); 
-		
-				// Create dropdown values a-z
-				$az_value = az_dropdown_value(0);
-				$az_output = az_dropdown_output(0);
-						   
-				$smarty->assign('az_value', $az_value);
-				$smarty->assign('az_output', $az_output);	
+while ($company_publisher = $sql_publisher->fetch_array(MYSQLI_BOTH))
+{
 
-		$smarty->assign("user_id",$_SESSION['user_id']);
+	$smarty->append('company_publisher',
+		 array('comp_id' => $company_publisher['pub_dev_id'],
+			   'comp_name' => $company_publisher['pub_dev_name']));
 
-		//Send all smarty variables to the templates
-		$smarty->display('file:../templates/0/games_main.html');
+}
+
+//Get Developer values to fill the searchfield
+$sql_developer = $mysqli->query("SELECT pub_dev.pub_dev_id,
+									 pub_dev.pub_dev_name
+									 FROM game_developer
+									 LEFT JOIN pub_dev ON (game_developer.dev_pub_id = pub_dev.pub_dev_id)
+									 GROUP BY pub_dev.pub_dev_id HAVING COUNT(DISTINCT pub_dev.pub_dev_id) = 1
+									 ORDER BY pub_dev.pub_dev_name ASC") 
+								or die("Problems retriving values from developers.");
+
+while ($company_developer = $sql_developer->fetch_array(MYSQLI_BOTH))
+{
+
+	$smarty->append('company_developer',
+		 array('comp_id' => $company_developer['pub_dev_id'],
+			   'comp_name' => $company_developer['pub_dev_name']));
+
+}
+
+//get the number of games in the archive
+$query_number = $mysqli->query("SELECT * FROM game") or die("Couldn't get the number of games");
+$v_rows = get_rows($query_number) or die("Couldn't get the number of games");
+
+$smarty->assign('games_nr', $v_rows); 
+
+		// Create dropdown values a-z
+		$az_value = az_dropdown_value(0);
+		$az_output = az_dropdown_output(0);
+				   
+		$smarty->assign('az_value', $az_value);
+		$smarty->assign('az_output', $az_output);	
+
+$smarty->assign("user_id",$_SESSION['user_id']);
+
+$smarty->assign('quick_search_games', 'quick_search_games_main');
+
+//Send all smarty variables to the templates
+$smarty->display('extends:../../../templates/html/admin/main.html|../../../templates/html/admin/frontpage.html|../../../templates/html/admin/games_main.html|../../../templates/html/admin/quick_search_games.html');
 
 //close the connection
 mysqli_close($mysqli);
