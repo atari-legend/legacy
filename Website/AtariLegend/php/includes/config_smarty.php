93fd5418ca95a1c2b631902decda02ff3ba47845
@stsilversurfer <silversurfer@atari-forum.com>
2015-11-22 21:12:15 +0100

    Move smarty folder from html to php as it is a php library.

--- Website/AtariLegend/php/includes/config_smarty.php
+++ Website/AtariLegend/php/includes/config_smarty.php
@@ -12,11 +12,11 @@
 
 //This file confirgures the smarty dirs
 
-require('../../../templates/html/includes/smarty/libs/Smarty.class.php');
+require('../../../php/includes/smarty/libs/Smarty.class.php');
 $smarty = new Smarty;
 
 $smarty->template_dir = '../../../templates/';
-$smarty->compile_dir = '../../../templates/html/includes/smarty/templates_c/';
-$smarty->config_dir = '../../../templates/html/includes/smarty/configs/';
-$smarty->cache_dir = '../../../templates/html/includes/smarty/cache/';
+$smarty->compile_dir = '../../../php/includes/smarty/templates_c/';
+$smarty->config_dir = '../../../php/includes/smarty/configs/';
+$smarty->cache_dir = '../../../php/includes/smarty/cache/';
 ?>
