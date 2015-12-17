38f46bfb5077f91c63fa6d8abda77cd54dc0c5e5
@stsilversurfer <silversurfer@atari-forum.com>
2015-12-17 22:35:57 +0100

    Cpanel, fixed paths and folders for game series

--- Website/AtariLegend/php/admin/games/games_series_add_games.php
+++ Website/AtariLegend/php/admin/games/games_series_add_games.php
@@ -19,7 +19,7 @@ Build game series page
 ***********************************************************************************
 */
 
-include("../includes/common.php");
+include("../../includes/common.php");
 
 	// SERIES LIST DROPDOWN
 				
@@ -88,5 +88,5 @@ include("../includes/common.php");
 				$smarty->assign('mySelect', 'num');
 
 //Send all smarty variables to the templates
-$smarty->display('file:../templates/0/games_series_add_games.html');
+$smarty->display('file:../../../templates/html/admin/games_series_add_games.html');
 ?>
