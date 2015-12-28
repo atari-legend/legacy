da70113043619fbc7970a3d6073a77b3b4c858dd
stgraveyard <martens_maarten@hotmail.com>
2015-12-29 00:36:43 +0100

    CPANEL Games section - GAME COMMENTS
    Game comments section completed in 3 skins - 1920 width
    
    2 remarks - @stsilversurfer need advice :
    - When deleting a comment --> Works perfect in IE, get an error in
    Firefox and Chrome (FETCH error)
    - We have french comments but our code can't handle 'é'. How do we solve
    this exactly?

--- Website/AtariLegend/php/admin/games/db_games_comment.php
+++ Website/AtariLegend/php/admin/games/db_games_comment.php
@@ -20,52 +20,48 @@ include("../../includes/common.php");
 include("../../includes/admin.php");
 
 if($action=="edit_games_comment")
-
 {
+	//****************************************************************************************
+	// This is the game comment edit place
+	//**************************************************************************************** 
 
-//****************************************************************************************
-// This is the game comment edit place
-//**************************************************************************************** 
-
-if (isset($comment_text) and isset($comment_id))
-{
+	if (isset($comment_text) and isset($comment_id))
+	{
 		$commentquery = $mysqli->query("UPDATE comments SET comment='$comment_text' WHERE comments_id='$comment_id'");
-}
+		$_SESSION['edit_message'] = "Comment edited";
+	}
 
-
-if ($view == "users_comments")
-{
-header("Location: ../games/games_comment.php?v_counter=$v_counter&c_counter=$c_counter&users_id=$users_id&view=$view");
-}
-else
-{
-header("Location: ../games/games_comment.php?v_counter=$v_counter");
-}
+	if ($view == "users_comments")
+	{
+		header("Location: ../games/games_comment.php?v_counter=$v_counter&c_counter=$c_counter&users_id=$users_id&view=$view");
+	}
+	else
+	{
+		header("Location: ../games/games_comment.php?v_counter=$v_counter");
+	}
 }
 
 
 // Delete
 if($action=="delete_comment")
-
 {
-
-//****************************************************************************************
-// This is the game comment edit place
-//**************************************************************************************** 
+	//****************************************************************************************
+	// This is the game comment edit place
+	//**************************************************************************************** 
 
 	if (isset($comment_id))
 	{
 		$sql = $mysqli->query("DELETE FROM game_user_comments WHERE comment_id = '$comment_id'") or die("couldn't delete game_comment quote");
 		$sql = $mysqli->query("DELETE FROM comments WHERE comments_id = '$comment_id'") or die("couldn't delete comment quote");
+		$_SESSION['edit_message'] = "Comment deleted";
 	}
 
-
-if ($view == "users_comments")
-{
-header("Location: ../games/games_comment.php?v_counter=$v_counter&c_counter=$c_counter&users_id=$users_id&view=$view");
-}
-else
-{
-header("Location: ../games/games_comment.php?v_counter=$v_counter");
-}
+	if ($view == "users_comments")
+	{
+	header("Location: ../games/games_comment.php?v_counter=$v_counter&c_counter=$c_counter&users_id=$users_id&view=$view");
+	}
+	else
+	{
+	header("Location: ../games/games_comment.php?v_counter=$v_counter");
+	}
 }
