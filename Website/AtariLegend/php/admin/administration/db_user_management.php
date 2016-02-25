e40607611c78029cec824c9e6cd1d659a424f4a1
@stsilversurfer <silversurfer@atari-forum.com>
2016-02-25 14:47:15 +0100

    Cpanel User Management - Added AL administrator filter - Added actual user delete function - Added counter and return message of how many users has been deleted. - Still a js reference error there but everything appear to be working

--- Website/AtariLegend/php/admin/administration/db_user_management.php
+++ Website/AtariLegend/php/admin/administration/db_user_management.php
@@ -19,14 +19,18 @@ include("../../includes/admin.php");
 // Ajax driven delete user query
 if (isset($action) and $action=="delete_user") {
 $start = microtime(true);
-
-				$time_elapsed_secs = microtime(true) - $start;
-				$smarty->assign("query_time",$time_elapsed_secs);
-
-		
-//Send all smarty variables to the templates
-
-echo "Lots of users deleted";
+$i=0;
+	foreach($user_id as $user)
+	{
+	$sql = $mysqli->query("DELETE FROM users WHERE user_id = '$user' ") or die ("error deleting user");
+				
+	$i++;
+	}	
+	
+	$time_elapsed_secs = microtime(true) - $start;
+	
+//No Smarty, send text directly to OSD
+echo "$i users deleted";
 
 }
 ?>
