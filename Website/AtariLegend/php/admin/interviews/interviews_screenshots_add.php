c3306efced1c0bd3f945c94011480f0883298cd3
mattiasl <mattias.lonnback@gmail.com>
2016-10-09 22:04:30 +0200

    removed function get_rows and replaced it in all places with num_rows

--- Website/AtariLegend/php/admin/interviews/interviews_screenshots_add.php
+++ Website/AtariLegend/php/admin/interviews/interviews_screenshots_add.php
@@ -9,64 +9,64 @@
 *
 *   Id:  interviews_screenshots_add.php,v 0.10 2005/07/30 23:07 Gatekeeper
 *   Id:  interviews_screenshots_add.php,v 0.20 2016/08/03 22:39 Gatekeeper
-*		- AL 2.0
+*   - AL 2.0
 *
 ***************************************************************************/
 
 //****************************************************************************************
 // This is the image selection/upload screen for the interviews
-//**************************************************************************************** 
+//****************************************************************************************
 
 include("../../includes/common.php");
 include("../../includes/admin.php");
 
 //load the search fields of the quick search side menu
-include("../../includes/quick_search_games.php"); 
+include("../../includes/quick_search_games.php");
 
 //Get the screenshots for this interview, if they exist
 $sql_screenshots = $mysqli->query("SELECT * FROM screenshot_interview
-			   		  			LEFT JOIN screenshot_main ON (screenshot_interview.screenshot_id = screenshot_main.screenshot_id)
-								WHERE screenshot_interview.interview_id = '$interview_id' ORDER BY screenshot_interview.screenshot_id")
-				   or die ("Database error - selecting screenshots");
+                    LEFT JOIN screenshot_main ON (screenshot_interview.screenshot_id = screenshot_main.screenshot_id)
+                WHERE screenshot_interview.interview_id = '$interview_id' ORDER BY screenshot_interview.screenshot_id")
+           or die ("Database error - selecting screenshots");
 
 //get the number of screenshots in the archive
-$v_screenshots = get_rows($sql_screenshots);
+$v_screenshots = $sql_screenshots->num_rows;
 $smarty->assign("screenshots_nr",$v_screenshots);
 
 $count = 1;
 
-while ( $screenshots=$sql_screenshots->fetch_array(MYSQLI_BOTH)) 
+while ( $screenshots=$sql_screenshots->fetch_array(MYSQLI_BOTH))
 {
-	// Get the image dimensions for the pop up window
-	$imginfo = getimagesize("$interview_screenshot_save_path$screenshots[screenshot_id].$screenshots[imgext]");
-	$width = $imginfo[0]+20;
-	$height = $imginfo[1]+25;
-	$image_path = "$interview_screenshot_path$screenshots[screenshot_id].$screenshots[imgext]";
-				
-	$smarty->append('screenshots',
-	    	 array('count' => $count,
-			 	   'width' => $width,
-				   'height' => $height,
-				   'image_path' => $image_path,
-				   'id' => $screenshots['screenshot_id']));
+  // Get the image dimensions for the pop up window
+  $imginfo = getimagesize("$interview_screenshot_save_path$screenshots[screenshot_id].$screenshots[imgext]");
+  $width = $imginfo[0]+20;
+  $height = $imginfo[1]+25;
+  $image_path = "$interview_screenshot_path$screenshots[screenshot_id].$screenshots[imgext]";
 
-	$count++;
-} 
+  $smarty->append('screenshots',
+         array('count' => $count,
+           'width' => $width,
+           'height' => $height,
+           'image_path' => $image_path,
+           'id' => $screenshots['screenshot_id']));
+
+  $count++;
+}
 
 //we need to get the data of the loaded interview
 $sql_interview = $mysqli->query("SELECT * FROM interview_main
-						   	  LEFT JOIN interview_text ON ( interview_main.interview_id = interview_text.interview_id ) 
-						   	  LEFT JOIN individuals on ( interview_main.ind_id = individuals.ind_id )
-							  WHERE interview_main.interview_id = '$interview_id'")
-				  or die ("Database error - selecting interview data");
+                  LEFT JOIN interview_text ON ( interview_main.interview_id = interview_text.interview_id )
+                  LEFT JOIN individuals on ( interview_main.ind_id = individuals.ind_id )
+                WHERE interview_main.interview_id = '$interview_id'")
+          or die ("Database error - selecting interview data");
 
 while ($interview = $sql_interview->fetch_array(MYSQLI_BOTH))
-{	
-	$smarty->assign('interview',
-	    	 array('interview_id' => $interview_id,
-				   'interview_ind_name' => $interview['ind_name'],
-				   'interview_author' => $interview['user_id'],
-				   'interview_ind_id' => $interview['ind_id']));
+{
+  $smarty->assign('interview',
+         array('interview_id' => $interview_id,
+           'interview_ind_name' => $interview['ind_name'],
+           'interview_author' => $interview['user_id'],
+           'interview_ind_id' => $interview['ind_id']));
 }
 
 $smarty->assign("user_id",$_SESSION['user_id']);
--- Website/AtariLegend/php/admin/links/link_addnew.php
+++ Website/AtariLegend/php/admin/links/link_addnew.php
@@ -6,7 +6,7 @@
 *   copyright            : (C) 2003 Atari Legend
 *   email                : silversurfer@atari-forum.com
 *   actual update        : file creation
-*							
+*
 *
 *   Id: link_addnew.php,v 0.10 2005/01/08 Silver Surfer
 *   Id: link_addnew.php,v 0.20 2015/09/07 ST Graveyard
@@ -25,12 +25,12 @@ include("../../includes/admin.php");
 
 $RESULT=$mysqli->query("SELECT * FROM website_category ORDER BY website_category_name");
 
-while ($rowlinkcat = $RESULT->fetch_array(MYSQLI_BOTH)) 
-{ 
-	$smarty->append('website_category',
-	array('website_category_id' => $rowlinkcat['website_category_id'],
-		  'website_category_name' => $rowlinkcat['website_category_name']));
-} 
+while ($rowlinkcat = $RESULT->fetch_array(MYSQLI_BOTH))
+{
+  $smarty->append('website_category',
+  array('website_category_id' => $rowlinkcat['website_category_id'],
+      'website_category_name' => $rowlinkcat['website_category_name']));
+}
 
 $smarty->assign("user_id",$_SESSION['user_id']);
 
@@ -38,41 +38,41 @@ $smarty->assign("user_id",$_SESSION['user_id']);
 // VALIDATE LINKS THAT HAVE SUBMITTED THROUGH THE FRONTPAGE
 //****************************************************************************************
 $sql = "SELECT * FROM website_validate
-		LEFT JOIN users ON (website_validate.user_id = users.user_id)";
+    LEFT JOIN users ON (website_validate.user_id = users.user_id)";
 
 $result = $mysqli->query($sql) or die("couldn't query website_validate");
 
-if (get_rows($result)<1)
-	{
-		$section = "Validate links";
-		$error_msg = "There are currently no new submitted links in the database to be validated.";
-	
-			$smarty->assign('error_msg',
-	    			  array('section' => $section,
-			  	  			'message' => $error_msg));	
-	}
-else 
-	{ 
-	  while ($valrow = $result->fetch_array(MYSQLI_BOTH))
- 		{
-		$link_sub = get_rows($mysqli->query("SELECT website_user_sub FROM website WHERE website_user_sub='$valrow[user_id]'"));
-		
-		$website_date = convert_timestamp($valrow['website_date']); 
-		$user_name = get_username_from_id($valrow['user_id']);
-				
-		$smarty->append('validate',
-			array('website_id' => $valrow['website_id'],
-				  'website_name' => $valrow['website_name'],
-				  'website_url' => $valrow['website_url'],
-				  'website_date' => $website_date,
-				  'website_category' => $valrow['website_category'],
-				  'website_description' => $valrow['website_description'],
-				  'user_email' => $valrow['email'],
-				  'link_sub' => $link_sub,
-				  'user_id' => $valrow['user_id'],
-				  'user_name' => $user_name));
-		} 
-	}
+if ($result->num_rows <1)
+  {
+    $section = "Validate links";
+    $error_msg = "There are currently no new submitted links in the database to be validated.";
+
+      $smarty->assign('error_msg',
+              array('section' => $section,
+                  'message' => $error_msg));
+  }
+else
+  {
+    while ($valrow = $result->fetch_array(MYSQLI_BOTH))
+    {
+    $link_sub = $mysqli->query("SELECT website_user_sub FROM website WHERE website_user_sub='$valrow[user_id]'")->num_rows;
+
+    $website_date = convert_timestamp($valrow['website_date']);
+    $user_name = get_username_from_id($valrow['user_id']);
+
+    $smarty->append('validate',
+      array('website_id' => $valrow['website_id'],
+          'website_name' => $valrow['website_name'],
+          'website_url' => $valrow['website_url'],
+          'website_date' => $website_date,
+          'website_category' => $valrow['website_category'],
+          'website_description' => $valrow['website_description'],
+          'user_email' => $valrow['email'],
+          'link_sub' => $link_sub,
+          'user_id' => $valrow['user_id'],
+          'user_name' => $user_name));
+    }
+  }
 
 //Send all smarty variables to the templates
 $smarty->display("file:".$cpanel_template_folder."link_addnew.html");
