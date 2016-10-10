024cd36ca43606bc6e29f924dcfea3be8bf57a54
mattiasl <mattias.lonnback@gmail.com>
2016-10-10 21:29:46 +0200

    fix alignment

--- Website/AtariLegend/php/admin/articles/articles_main.php
+++ Website/AtariLegend/php/admin/articles/articles_main.php
@@ -8,7 +8,7 @@
 *   actual update        : Start of creation file
 *
 *   Id: interviews_main.php,v 0.10 05/10/2016 23:06 Gatekeeper
-*   - AL 2.0 / creation of section
+*             - AL 2.0 / creation of section
 *
 ***************************************************************************/
 
@@ -24,15 +24,15 @@ include("../../includes/quick_search_games.php");
 
 //Get list of all articles
 $sql_articles = $mysqli->query("SELECT * FROM article_main
-                LEFT JOIN article_text ON (article_main.article_id = article_text.article_id)
-                ORDER BY article_text.article_date ASC")
-             or die ("Couldn't query articles database");
+                                LEFT JOIN article_text ON (article_main.article_id = article_text.article_id)
+                                ORDER BY article_text.article_date ASC")
+                                or die ("Couldn't query articles database");
 
 while ($articles = $sql_articles->fetch_array(MYSQLI_BOTH))
 {
   $smarty->append('articles',
-         array('article_id' => $articles['article_id'],
-           'article_title' => $articles['article_title']));
+            array('article_id' => $articles['article_id'],
+                  'article_title' => $articles['article_title']));
 }
 
 if ( isset($action) and $action == 'search' )
@@ -41,21 +41,21 @@ if ( isset($action) and $action == 'search' )
     {
       //show all
       $sql_articles = $mysqli->query("SELECT * FROM article_main
-                      LEFT JOIN article_text on (article_main.article_id = article_text.article_id)
-                      LEFT JOIN article_type on (article_main.article_type_id = article_type.article_type_id)
-                      LEFT JOIN users on ( article_main.user_id = users.user_id )
-                      ORDER BY article_text.article_date DESC")
-                 or die ("Couldn't query database for articles");
+                                      LEFT JOIN article_text on (article_main.article_id = article_text.article_id)
+                                      LEFT JOIN article_type on (article_main.article_type_id = article_type.article_type_id)
+                                      LEFT JOIN users on ( article_main.user_id = users.user_id )
+                                      ORDER BY article_text.article_date DESC")
+                                      or die ("Couldn't query database for articles");
     }
     else
     {
       $sql_articles = $mysqli->query("SELECT * FROM article_main
-                      LEFT JOIN article_text on (article_main.article_id = article_text.article_id)
-                      LEFT JOIN article_type on (article_main.article_type_id = article_type.article_type_id)
-                      LEFT JOIN users on ( article_main.user_id = users.user_id )
-                      WHERE article_main.article_id = '$article_search'
-                      ORDER BY article_text.article_date DESC")
-                 or die ("Couldn't query database for articles");
+                                      LEFT JOIN article_text on (article_main.article_id = article_text.article_id)
+                                      LEFT JOIN article_type on (article_main.article_type_id = article_type.article_type_id)
+                                      LEFT JOIN users on ( article_main.user_id = users.user_id )
+                                      WHERE article_main.article_id = '$article_search'
+                                      ORDER BY article_text.article_date DESC")
+                                      or die ("Couldn't query database for articles");
     }
 
     //get the number of articles in the archive
@@ -77,13 +77,13 @@ if ( isset($action) and $action == 'search' )
       $article_title = rawurlencode($article['article_title']);
 
       $smarty->append('article_selected',
-           array('user_id' => $article['userid'],
-               'user_email' => $article['email'],
-               'article_id' => $article['article_id'],
-               'article_title' => $article['article_title'],
-               'article_date' => $article_date,
-               'article_type' => $article['article_type'],
-               'article_text' => $article_text));
+          array('user_id' => $article['userid'],
+                'user_email' => $article['email'],
+                'article_id' => $article['article_id'],
+                'article_title' => $article['article_title'],
+                'article_date' => $article_date,
+                'article_type' => $article['article_type'],
+                'article_text' => $article_text));
   }
 }
 
@@ -94,3 +94,4 @@ $smarty->display("file:".$cpanel_template_folder."articles_main.html");
 
 //close the connection
 mysqli_close($mysqli);
+?>
