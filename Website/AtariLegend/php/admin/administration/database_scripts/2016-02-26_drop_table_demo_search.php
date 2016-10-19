9e23d66daad18bac15a0225395727c1ef87fdcf6
mattiasl <mattias.lonnback@gmail.com>
2016-10-19 23:01:15 +0200

    Fixing db updater. -added or die, which was missing. -Fixed alot of the scripts which wouldn't execute properly * more work needed on this.

--- Website/AtariLegend/php/admin/administration/database_scripts/2016-02-26_drop_table_demo_search.php
+++ Website/AtariLegend/php/admin/administration/database_scripts/2016-02-26_drop_table_demo_search.php
@@ -5,32 +5,32 @@
 *   begin                : 2016-02-26
 *   copyright            : (C) 2016 Atari Legend
 *   email                : silversurfer@atari-forum.com
-*   actual update        : 	
-*							 
-*							
+*   actual update        :
+*
+*
 *
 *   Id: 2016-02-26_drop_table_game_search.php,v 0.10 2016-02-26 Silver Surfer
 *
 ***************************************************************************/
 
 // Unique identifier set by developer.
-$database_update_id = 25; 
+$database_update_id = 25;
 
 // Description of what the change will do.
-$update_description = "Drop Table demo_search"; 
+$update_description = "Drop Table demo_search";
 
 // Should the database change query execute if test is "test_fail" or "test_success"
 $execute_condition = "test_success";
 
 //This is the test query, the query should be made to get an either true or false result.
-$test_condition = "SELECT * FROM information_schema.tables 
+$test_condition = "SELECT * FROM information_schema.tables
 WHERE table_schema = '$db_databasename' AND table_name = 'demo_search' LIMIT 1";
 
-// Database change 
+// Database change
 $database_update_sql = "DROP TABLE `demo_search`";
 
-// If the update should auto execute without user interaction set to "yes".    
-$database_autoexecute = "no";
+// If the update should auto execute without user interaction set to "yes".
+$database_autoexecute = "yes";
 
 // This var should almost allways be set to "no", it is only used for certain corner cases where
 // the database change has already been done in some other way and we only want to update the
--- Website/AtariLegend/php/admin/administration/database_scripts/2016-10-14_change_fields_doc_disk_tool.php
+++ Website/AtariLegend/php/admin/administration/database_scripts/2016-10-14_change_fields_doc_disk_tool.php
@@ -5,28 +5,28 @@
 *   begin                : 2016-10-14
 *   copyright            : (C) 2016 Atari Legend
 *   email                : martens_maarten@hotmail.com
-*   actual update        : 	
-*							 
+*   actual update        :
+*
 *   Id: 2016-10-14_change_fields_doc_disk_tool.php,v 0.10 2016-10-14 STG
 *
 ***************************************************************************/
 
 // Unique identifier set by developer.
-$database_update_id = 42; 
+$database_update_id = 42;
 
 // Description of what the change will do.
-$update_description = "Change id fields of doc_disk_tool"; 
+$update_description = "Change id fields of doc_disk_tool";
 
 // Should the database change query execute if test is "test_fail" or "test_success"
 $execute_condition = "test_success";
 
 //This is the test query, the query should be made to get an either true or false result.
-$test_condition = "SELECT * FROM information_schema.columns 
+$test_condition = "SELECT * FROM information_schema.columns
 WHERE table_schema = '$db_databasename' AND table_name = 'doc_disk_game' LIMIT 1";
 
-// Database change 
-$database_update_sql = "ALTER TABLE `doc_disk_tools` CHANGE `doc_tools_id` `doc_disk_tool_id` INT(11) NOT NULL AUTO_INCREMENT, CHANGE `doc_type_id` `doc_id` INT(11) NULL DEFAULT NULL;";
-// If the update should auto execute without user interaction set to "yes".    
+// Database change
+$database_update_sql = "ALTER TABLE `doc_disk_tool` CHANGE `doc_tools_id` `doc_disk_tool_id` INT(11) NOT NULL AUTO_INCREMENT, CHANGE `doc_type_id` `doc_id` INT(11) NULL DEFAULT NULL;";
+// If the update should auto execute without user interaction set to "yes".
 $database_autoexecute = "yes";
 
 // This var should almost allways be set to "no", it is only used for certain corner cases where
