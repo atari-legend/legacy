0bf75aabe022f67dc816b99cd9e05371653301d8
mattiasl <mattias.lonnback@gmail.com>
2016-10-22 20:14:09 +0200

    Minor tweaks to php code

--- Website/AtariLegend/php/admin/menus/menus_type_edit.php
+++ Website/AtariLegend/php/admin/menus/menus_type_edit.php
@@ -5,7 +5,7 @@
 *   begin                : September 04, 2016
 *   copyright            : (C) 2016 Atari Legend
 *   email                : admin@atarilegend.com
-*						   Created file
+*                          Created file
 *
 ***************************************************************************/
 
@@ -14,7 +14,7 @@ include("../../includes/common.php");
 include("../../includes/admin.php");
 
 //load the search fields of the quick search side menu
-include("../../includes/quick_search_games.php"); 
+include("../../includes/quick_search_games.php");
 
 /*
 ************************************************************************************************
@@ -22,41 +22,38 @@ This is the menus search list page
 ************************************************************************************************
 */
 //Check if we seclected a menu type
-if	($menu_type_id == '-' or $menu_type_id == ' ')
+if  ($menu_type_id == '-' or $menu_type_id == ' ')
 {
-	$_SESSION['edit_message'] = "Please select a menu type";
-	header("Location: ../menus/menus_type.php");
+    $_SESSION['edit_message'] = "Please select a menu type";
+    header("Location: ../menus/menus_type.php");
 }
 else
 {
-	//get all the menu types for dropdown
-	$sql_menus_type = "SELECT * FROM menu_types_main";
+    //get all the menu types for dropdown
+    $result_menus_type= $mysqli->query("SELECT * FROM menu_types_main");
 
-	$result_menus_type= $mysqli->query($sql_menus_type);
+    $rows = $result_menus_type->num_rows;
+    while ( $row=$result_menus_type->fetch_array(MYSQLI_BOTH) )
+    {
+        $smarty->append('menus_type',
+         array('menu_types_main_id' => $row['menu_types_main_id'],
+               'menu_type_text' => $row['menu_types_text']));
+    }
+    $smarty->assign("user_id",$_SESSION['user_id']);
 
-	$rows = $result_menus_type->num_rows;
-	while ( $row=$result_menus_type->fetch_array(MYSQLI_BOTH) ) 
-	{  
-		$smarty->append('menus_type',
-		 array('menu_types_main_id' => $row['menu_types_main_id'],
-			   'menu_type_text' => $row['menu_types_text']));	
-	}	
-	$smarty->assign("user_id",$_SESSION['user_id']);
+    $result_menus_type_edit= $mysqli->query("SELECT * FROM menu_types_main WHERE menu_types_main_id = $menu_type_id") or die ("error getting menu type");
 
-	$sql_menus_type_edit = "SELECT * FROM menu_types_main WHERE menu_types_main_id = $menu_type_id";
-	$result_menus_type_edit= $mysqli->query($sql_menus_type_edit) or die ("error getting menu type");
+    while ( $row=$result_menus_type_edit->fetch_array(MYSQLI_BOTH) )
+    {
+        $smarty->assign('menus_type_edit',
+        array('menu_types_main_id_edit' => $row['menu_types_main_id'],
+              'menu_type_text_edit' => $row['menu_types_text']));
+    }
 
-	while ( $row=$result_menus_type_edit->fetch_array(MYSQLI_BOTH) ) 
-	{
-		$smarty->assign('menus_type_edit',
-		array('menu_types_main_id_edit' => $row['menu_types_main_id'],
-			  'menu_type_text_edit' => $row['menu_types_text']));	
-	}
-
-	//Send all smarty variables to the templates
-	$smarty->display("file:".$cpanel_template_folder."menus_type_edit.html");
+    //Send all smarty variables to the templates
+    $smarty->display("file:".$cpanel_template_folder."menus_type_edit.html");
 }
 
 //close the connection
-mysqli_free_result($result_menus_type);	
+mysqli_free_result($result_menus_type);
 ?>
--- Website/AtariLegend/php/admin/user/db_user.php
+++ Website/AtariLegend/php/admin/user/db_user.php
@@ -1,23 +1,23 @@
 <?php
 /***************************************************************************
-*                                db_user.php
-*                            -----------------------
-*   begin                : friday, November 11, 2005
-*   copyright            : (C) 2005 Atari Legend
-*   email                : maarten.martens@freebel.net
-*   actual update        : file creation
-*
-*   Id: db_user.php,v 1.01 2015/12/21 ST Graveyard - Added messages
-*                          - Added more SQL statements
-*   Id: db_user.php,v 1.02 2016/08/19 ST Graveyard - Added Change log
-*
-***************************************************************************/
+ *                                db_user.php
+ *                            -----------------------
+ *   begin                : friday, November 11, 2005
+ *   copyright            : (C) 2005 Atari Legend
+ *   email                : maarten.martens@freebel.net
+ *   actual update        : file creation
+ *
+ *   Id: db_user.php,v 1.01 2015/12/21 ST Graveyard - Added messages
+ *                          - Added more SQL statements
+ *   Id: db_user.php,v 1.02 2016/08/19 ST Graveyard - Added Change log
+ *
+ ***************************************************************************/
 
 /*
-***********************************************************************************
-This is the database modification area for the cpanel
-***********************************************************************************
-*/
+ ***********************************************************************************
+ This is the database modification area for the cpanel
+ ***********************************************************************************
+ */
 // include common variables and functions
 include("../../includes/common.php");
 include("../../includes/admin.php");
@@ -25,193 +25,152 @@ include("../../includes/admin.php");
 //****************************************************************************************
 // add avatar
 //****************************************************************************************
-if (isset($action) and $action == 'avatar_upload')
-{
-  $image = $_FILES['image'];
+if (isset($action) and $action == 'avatar_upload') {
+    $image = $_FILES['image'];
 
-  $tmp_name=$image['tmp_name'];
+    $tmp_name = $image['tmp_name'];
 
-  if ($tmp_name!=='none')
-  {
-    // Check what extention the file has and if it is allowed.
-    $ext="";
-    $type_image = $image['type'];
+    if ($tmp_name !== 'none') {
+        // Check what extention the file has and if it is allowed.
+        $ext        = "";
+        $type_image = $image['type'];
 
-    // set extension
-    if ( $type_image=='image/x-png')
-    {
-      $ext='png';
-    }
-    elseif ( $type_image=='image/png')
-    {
-      $ext='png';
-    }
-    elseif ( $type_image=='image/gif')
-    {
-      $ext='gif';
-    }
-    elseif ( $type_image=='image/jpeg')
-    {
-      $ext='jpg';
-    }
+        // set extension
+        if ($type_image == 'image/x-png') {
+            $ext = 'png';
+        } elseif ($type_image == 'image/png') {
+            $ext = 'png';
+        } elseif ($type_image == 'image/gif') {
+            $ext = 'gif';
+        } elseif ($type_image == 'image/jpeg') {
+            $ext = 'jpg';
+        }
 
-      if ($ext!=="")
-      {
-        // Rename the uploaded file to the users id number and move it to its proper place.
-        $mysqli->query("UPDATE users SET avatar_ext='$ext' WHERE user_id='$user_id_selected'");
-        $file_data = rename("$tmp_name", "$user_avatar_save_path$user_id_selected.$ext");
-        chmod("$user_avatar_save_path$user_id_selected.$ext", 0777);
-      }
+        if ($ext !== "") {
+            // Rename the uploaded file to the users id number and move it to its proper place.
+            $mysqli->query("UPDATE users SET avatar_ext='$ext' WHERE user_id='$user_id_selected'");
+            $file_data = rename("$tmp_name", "$user_avatar_save_path$user_id_selected.$ext");
+            chmod("$user_avatar_save_path$user_id_selected.$ext", 0777);
+        }
 
-    // check for size specs
-    $imginfo = getimagesize("$user_avatar_save_path$user_id_selected.$ext") or die("getimagesize not working");
-    $width = $imginfo[0];
-    $height = $imginfo[1];
+        // check for size specs
+        $imginfo = getimagesize("$user_avatar_save_path$user_id_selected.$ext") or die("getimagesize not working");
+        $width  = $imginfo[0];
+        $height = $imginfo[1];
 
-    if ($width<101 and $height<101)
-    {
-      $_SESSION['edit_message'] = "Avatar added";
-      create_log_entry('Users', $user_id_selected, 'Avatar', $user_id_selected, 'Insert', $_SESSION['user_id']);
+        if ($width < 101 and $height < 101) {
+            $_SESSION['edit_message'] = "Avatar added";
+            create_log_entry('Users', $user_id_selected, 'Avatar', $user_id_selected, 'Insert', $_SESSION['user_id']);
+        } else {
+            $_SESSION['edit_message'] = "Upload failed due to not confirming to specs.";
+            $mysqli->query("UPDATE users SET avatar_ext='' WHERE user_id='$user_id_selected'");
+            unlink("$user_avatar_save_path$user_id_selected.$ext");
+        }
     }
-    else
-    {
-      $_SESSION['edit_message'] = "Upload failed due to not confirming to specs.";
-      $mysqli->query("UPDATE users SET avatar_ext='' WHERE user_id='$user_id_selected'");
-      unlink ("$user_avatar_save_path$user_id_selected.$ext");
-    }
-  }
 }
 
 //****************************************************************************************
 // delete avatar
 //****************************************************************************************
-if (isset($action) and $action=="delete_avatar")
-{
+if (isset($action) and $action == "delete_avatar") {
+    $avatar_query = $mysqli->query("SELECT avatar_ext FROM users WHERE user_id='$user_id_selected'");
+    list($avatar_ext) = $avatar_query->fetch_array(MYSQLI_BOTH);
 
-$sql = "SELECT avatar_ext FROM users WHERE user_id='$user_id_selected'";
-  $avatar_query = $mysqli->query($sql);
-  list ($avatar_ext) = $avatar_query->fetch_array(MYSQLI_BOTH);
+    $mysqli->query("UPDATE users SET avatar_ext='' WHERE user_id='$user_id_selected'");
+    $_SESSION['edit_message'] = "Avatar deleted";
+    unlink("$user_avatar_save_path$user_id_selected.$avatar_ext");
 
-  $mysqli->query("UPDATE users SET avatar_ext='' WHERE user_id='$user_id_selected'");
-  $_SESSION['edit_message'] = "Avatar deleted";
-  unlink ("$user_avatar_save_path$user_id_selected.$avatar_ext");
-
-  create_log_entry('Users', $user_id_selected, 'Avatar', $user_id_selected, 'Delete', $_SESSION['user_id']);
+    create_log_entry('Users', $user_id_selected, 'Avatar', $user_id_selected, 'Delete', $_SESSION['user_id']);
 
 }
 
 //****************************************************************************************
 // reset pwd
 //****************************************************************************************
-if (isset($action) and $action == 'reset_pwd')
-{
-  $mysqli->query("UPDATE users SET password='' WHERE user_id='$user_id_selected'");
-  $_SESSION['edit_message'] = "Password reset";
+if (isset($action) and $action == 'reset_pwd') {
+    $mysqli->query("UPDATE users SET password='' WHERE user_id='$user_id_selected'");
+    $_SESSION['edit_message'] = "Password reset";
 
-  create_log_entry('Users', $user_id_selected, 'User', $user_id_selected, 'Update', $_SESSION['user_id']);
+    create_log_entry('Users', $user_id_selected, 'User', $user_id_selected, 'Update', $_SESSION['user_id']);
 }
 
 //****************************************************************************************
 // modify user
 //****************************************************************************************
-if (isset($action) and $action == 'modify_user')
-{
-  if ( isset($user_pwd) && $user_pwd != '' )
-  {
-      $md5pass = md5($user_pwd);
-      $mysqli->query("UPDATE users SET userid='$user_name', password='$md5pass', email='$user_email', permission='$user_permission', user_website='$user_website', user_icq='$user_icq', user_msnm='$user_msnm', user_aim='$user_aim', inactive='$user_inactive' WHERE user_id='$user_id_selected'");
-      $_SESSION['edit_message'] = "User data modified";
+if (isset($action) and $action == 'modify_user') {
+    if (isset($user_pwd) && $user_pwd != '') {
+        $md5pass = md5($user_pwd);
+        $mysqli->query("UPDATE users SET userid='$user_name', password='$md5pass', email='$user_email', permission='$user_permission', user_website='$user_website', user_icq='$user_icq', user_msnm='$user_msnm', user_aim='$user_aim', inactive='$user_inactive' WHERE user_id='$user_id_selected'");
+        $_SESSION['edit_message'] = "User data modified";
 
-      create_log_entry('Users', $user_id_selected, 'User', $user_id_selected, 'Update', $_SESSION['user_id']);
-  }
-  else
-  {
-      $mysqli->query("UPDATE users SET userid='$user_name', email='$user_email', permission='$user_permission', user_website='$user_website', user_icq='$user_icq', user_msnm='$user_msnm', user_aim='$user_aim', inactive='$user_inactive' WHERE user_id='$user_id_selected'");
-      $_SESSION['edit_message'] = "User data modified";
+        create_log_entry('Users', $user_id_selected, 'User', $user_id_selected, 'Update', $_SESSION['user_id']);
+    } else {
+        $mysqli->query("UPDATE users SET userid='$user_name', email='$user_email', permission='$user_permission', user_website='$user_website', user_icq='$user_icq', user_msnm='$user_msnm', user_aim='$user_aim', inactive='$user_inactive' WHERE user_id='$user_id_selected'");
+        $_SESSION['edit_message'] = "User data modified";
 
-      create_log_entry('Users', $user_id_selected, 'User', $user_id_selected, 'Update', $_SESSION['user_id']);
-  }
+        create_log_entry('Users', $user_id_selected, 'User', $user_id_selected, 'Update', $_SESSION['user_id']);
+    }
 }
 
 //****************************************************************************************
 // delete user
 //****************************************************************************************
-if (isset($action) and $action == 'delete_user')
-{
-  //First we need to do a hell of a lot checks before we can delete an actual user.
-  $sql = $mysqli->query("SELECT * FROM comments
+if (isset($action) and $action == 'delete_user') {
+    //First we need to do a hell of a lot checks before we can delete an actual user.
+    $sql = $mysqli->query("SELECT * FROM comments
             LEFT JOIN game_user_comments ON (comments.comments_id = game_user_comments.comment_id)
             LEFT JOIN game ON ( game_user_comments.game_id = game.game_id )
-            WHERE comments.user_id = '$user_id_selected'") or die ("error selecting game comments");
+            WHERE comments.user_id = '$user_id_selected'") or die("error selecting game comments");
 
-  if ( $sql->num_rows > 0 )
-  {
-    $_SESSION['edit_message'] = 'Deletion failed - This user has submitted game comments - Delete it in the appropriate section';
-  }
-  else
-  {
-    $sql = $mysqli->query("SELECT * FROM review_main
+    if ($sql->num_rows > 0) {
+        $_SESSION['edit_message'] = 'Deletion failed - This user has submitted game comments - Delete it in the appropriate section';
+    } else {
+        $sql = $mysqli->query("SELECT * FROM review_main
               LEFT JOIN review_game ON (review_main.review_id = review_game.review_id)
               LEFT JOIN game ON ( review_game.game_id = game.game_id )
-              WHERE review_main.user_id = '$user_id_selected'") or die ("error selecting game reviews");
+              WHERE review_main.user_id = '$user_id_selected'") or die("error selecting game reviews");
 
-    if ( $sql->num_rows > 0 )
-    {
-      $_SESSION['edit_message'] = 'Deletion failed - This user has submitted game reviews - Delete it in the appropriate section';
-    }
-    else
-    {
-      $sql = $mysqli->query("SELECT * FROM game_submitinfo
-                  LEFT JOIN game ON (game_submitinfo.game_id = game.game_id)
-                WHERE user_id = '$user_id_selected'") or die ("error selecting game info");
-      if ( $sql->num_rows > 0 )
-      {
-        $_SESSION['edit_message'] = 'Deletion failed - This user has submitted game info - Delete it in the appropriate section';
-      }
-      else
-      {
-        $sql = $mysqli->query("SELECT * FROM website WHERE user_id = '$user_id_selected'") or die ("error selecting links");
+        if ($sql->num_rows > 0) {
+            $_SESSION['edit_message'] = 'Deletion failed - This user has submitted game reviews - Delete it in the appropriate section';
+        } else {
+            $sql = $mysqli->query("SELECT * FROM game_submitinfo
+                    LEFT JOIN game ON (game_submitinfo.game_id = game.game_id)
+                    WHERE user_id = '$user_id_selected'") or die("error selecting game info");
+            if ($sql->num_rows > 0) {
+                $_SESSION['edit_message'] = 'Deletion failed - This user has submitted game info - Delete it in the appropriate section';
+            } else {
+                $sql = $mysqli->query("SELECT * FROM website WHERE user_id = '$user_id_selected'") or die("error selecting links");
 
-        if ( $sql->num_rows > 0 )
-        {
-          $_SESSION['edit_message'] = 'Deletion failed - This user has submitted links - Delete it in the appropriate section';
-        }
-        else
-        {
-          $sql = $mysqli->query("SELECT * FROM news WHERE user_id = '$user_id_selected'") or die ("error selecting news");
+                if ($sql->num_rows > 0) {
+                    $_SESSION['edit_message'] = 'Deletion failed - This user has submitted links - Delete it in the appropriate section';
+                } else {
+                    $sql = $mysqli->query("SELECT * FROM news WHERE user_id = '$user_id_selected'") or die("error selecting news");
 
-          if ( $sql->num_rows > 0 )
-          {
-            $_SESSION['edit_message'] = 'Deletion failed - This user has submitted news updates - Delete it in the appropriate section';
-          }
-          else
-          {
-            $sql = $mysqli->query("SELECT * FROM comments
-            LEFT JOIN demo_user_comments ON (comments.comments_id = demo_user_comments.comments_id)
-            LEFT JOIN demo ON ( demo_user_comments.demo_id = demo.demo_id )
-            WHERE comments.user_id = $user_id_selected") or die ("error selecting demo comments");
+                    if ($sql->num_rows > 0) {
+                        $_SESSION['edit_message'] = 'Deletion failed - This user has submitted news updates - Delete it in the appropriate section';
+                    } else {
+                        $sql = $mysqli->query("SELECT * FROM comments
+                        LEFT JOIN demo_user_comments ON (comments.comments_id = demo_user_comments.comments_id)
+                        LEFT JOIN demo ON ( demo_user_comments.demo_id = demo.demo_id )
+                        WHERE comments.user_id = $user_id_selected") or die("error selecting demo comments");
 
-            if ( $sql->num_rows > 0 )
-            {
-              $_SESSION['edit_message'] = 'Deletion failed - This user has submitted demo comments - Delete it in the appropriate section';
+                        if ($sql->num_rows > 0) {
+                            $_SESSION['edit_message'] = 'Deletion failed - This user has submitted demo comments - Delete it in the appropriate section';
+                        } else {
+                            create_log_entry('Users', $user_id_selected, 'User', $user_id_selected, 'Delete', $_SESSION['user_id']);
+
+                            $mysqli->query("DELETE from users WHERE user_id='$user_id_selected'") or die('deleting user failed');
+
+                            $_SESSION['edit_message'] = 'User deleted succesfully';
+                        }
+                    }
+                }
             }
-            else
-            {
-              create_log_entry('Users', $user_id_selected, 'User', $user_id_selected, 'Delete', $_SESSION['user_id']);
-
-              $mysqli->query("DELETE from users WHERE user_id='$user_id_selected'") or die ('deleting user failed');
-
-              $_SESSION['edit_message'] = 'User deleted succesfully';
-            }
-          }
         }
-      }
     }
-  }
 }
 
 header("Location: ../user/user_detail.php?user_id_selected=$user_id_selected");
-
 
 //close the connection
 mysqli_close($mysqli);
