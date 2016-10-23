1171fb9a0ee9dd6e576c07ca2eeacb855d232cbd
stgraveyard <martens_maarten@hotmail.com>
2016-10-23 09:12:12 +0200

    CPANEL update - Help
    Finished adding help pop ups in the menu section and starten working on
    a main help file (which I will also be adding in each section)

---
+++ Website/AtariLegend/php/admin/administration/technical_doc.php
@@ -0,0 +1,22 @@
+<?php
+/***************************************************************************
+*                                technical_doc.php
+*                            -----------------------
+*   begin                : Friday, October 21, 2016
+*   copyright            : (C) 2016 Atari Legend
+*   email                : admin@atarilegend.com
+*
+* 	id : technical_doc.php ,v 0.10 2016/10/21 ST Graveyard 14:23
+*			- AL 2.0
+*
+***************************************************************************/
+
+include("../../includes/common.php");
+include("../../includes/admin.php"); 
+
+//load the search fields of the quick search side menu
+include("../../includes/quick_search_games.php");  
+	
+//Send all smarty variables to the templates
+$smarty->display("file:".$cpanel_template_folder."technical_doc.html");
+?>
