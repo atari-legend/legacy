8fd065f61f5d25355c72b9af6ec4495c1e663a58
mattiasl <mattias.lonnback@gmail.com>
2016-11-05 12:27:13 +0100

    Style 2 didn't compile correctly a dot in the wrong place

--- Website/AtariLegend/php/admin/index.php
+++ Website/AtariLegend/php/admin/index.php
@@ -5,12 +5,11 @@
 *   begin                : Sunday, Aug 31, 2003
 *   copyright            : (C) 2003 Atari Legend
 *   email                : maarten.martens@freebel.net
-*   actual update        : added delete screenshot comment case, 
-*						   review preview case
+*   actual update        : added delete screenshot comment case,
+*                          review preview case
 *
 *   Id: index.php,v 0.20 2003/08/29 14:56 Gatekeeper
 *
 ***************************************************************************/
 
 header("Location: administration/statistics.php");
-?>
