27c2f573db58ad80c5a3b26bd6f62222fa6bf496
mattiasl <mattias.lonnback@gmail.com>
2017-01-21 20:48:43 +0100

    Lib folder
    Created lib folder for our own functions and classes.

--- Website/AtariLegend/php/config/common.php
+++ Website/AtariLegend/php/config/common.php
@@ -19,9 +19,9 @@ extract($_REQUEST);
 include("../../config/connect.php");
 include("../../../php/vendor/smarty/smarty/libs/Smarty.class.php");
 include("../../config/config.php");
-include("../../includes/user_functions.php");
-include("../../includes/functions.php");
-include("../../includes/karma.php");
+include("../../lib/user_functions.php");
+include("../../lib/functions.php");
+include("../../lib/karma.php");
 
 if (file_exists("../../config/database_upgrade.php")==true) { exit("Upgrade mode");}
 
