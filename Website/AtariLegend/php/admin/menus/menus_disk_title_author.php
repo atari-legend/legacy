4de07dc407f0c44356da86115041526b81ef3f65
stgraveyard <martens_maarten@hotmail.com>
2017-02-06 09:43:49 +0100

    Early work - adding individuals to menu title
    Hello Mattias,
    Maybe if you have some time. This is a work in progress, but I'm a bit stuck with the Java. Maybe you can help?
    - First you need to run the db update section, I've added a script
    - Than go to the menu section. Add a game to a menu. Press the game name from the list. A pop up will apear where you can add authors. When pressing add, the DB functions are ok, but the redirect logs me out. All I want is when pressing add, the pop up is refreshed. Any idea?

---
+++ Website/AtariLegend/php/admin/menus/menus_disk_title_author.php
@@ -0,0 +1,107 @@
+<?php
+/***************************************************************************
+ *                                menus_disk_title_author.php
+ *                            ----------------------------------
+ *   begin                : 05 February 2017
+ *   copyright            : (C) 2017 Atari Legend
+ *   email                : admin@atarilegend.com
+ *                         Created file - STG
+ *
+ *  id : menus_disk_title_author.php ,v 0.10 2017/02/05 ST Graveyard 22:38
+ *          - AL 2.0
+ *
+ ***************************************************************************
+ 
+************************************************************************************************
+On this page we link authors to a menu disk title
+************************************************************************************************/
+
+//load all common functions
+include("../../config/common.php");
+include("../../config/admin.php");
+
+//Get the individuals
+$sql_individuals = $mysqli->query("SELECT * FROM individuals ORDER BY ind_name ASC");
+
+while ($individuals = $sql_individuals->fetch_array(MYSQLI_BOTH)) {
+    if ($individuals['ind_name'] != '') {
+        $smarty->append('ind_gene', array(
+            'ind_id' => $individuals['ind_id'],
+            'ind_name' => $individuals['ind_name']
+        ));
+    }
+}
+
+//load the authors for this title
+$sql_author_info = "SELECT  individuals.ind_id,
+                            individuals.ind_name,
+                            author_type.author_type_info
+                            FROM menu_disk_title_author
+                            LEFT JOIN individuals ON (individuals.ind_id = menu_disk_title_author.ind_id)
+                            LEFT JOIN author_type ON (menu_disk_title_author.author_type_id = author_type.author_type_id)
+                            WHERE menu_disk_title_author.menu_disk_title_id = '$menu_disk_title_id'
+                            ORDER BY individuals.ind_name ASC";
+
+$query_author_info = $mysqli->query($sql_author_info) or die ("problem getting author info");
+
+while ($query = $query_author_info->fetch_array(MYSQLI_BOTH)) {
+     
+    $sql_ind_nicks = $mysqli->query("SELECT nick_id FROM individual_nicks WHERE ind_id = '$query[ind_id]'");
+        
+    while ($fetch_ind_nicks = $sql_ind_nicks->fetch_array(MYSQLI_BOTH)) {
+     
+        $nick_id = $fetch_ind_nicks['nick_id'];
+        
+        $sql_nick_names = $mysqli->query("SELECT ind_name from individuals WHERE ind_id = '$nick_id'") or die('Error: ' . mysqli_error($mysqli));
+    
+        while ($fetch_nick_names = $sql_nick_names->fetch_array(MYSQLI_BOTH)) {
+
+            $smarty->append('ind_nick', array(
+                'ind_id' => $query['ind_id'],
+                'individual_nicks_id' => $nick_id,
+                'nick' => $fetch_nick_names['ind_name']
+            ));
+        }
+    }  
+
+    // This smarty is used for for the menu_disk_title credits
+    $smarty->append('title_credits', array(
+        'ind_id' => $query['ind_id'],
+        'ind_name' => $query['ind_name'],
+        'author_type_info' => $query['author_type_info']
+    ));
+}
+
+// Get Author types for
+$sql_author_types = "SELECT * FROM author_type ORDER BY author_type_info ASC";
+$query_author = $mysqli->query($sql_author_types) or die('Error: ' . mysqli_error($mysqli));
+
+while ($author_ind = $query_author->fetch_array(MYSQLI_BOTH)) {
+    $smarty->append('author_type', array(
+        'author_type_id' => $author_ind['author_type_id'],
+        'author_type_info' => $author_ind['author_type_info']
+    ));
+}
+
+$smarty->assign('menu_disk_title_id', $menu_disk_title_id);
+
+if ($action == 'game_load')
+{
+    //get the game name
+    $sql_game_name = $mysqli->query("SELECT * FROM game WHERE game_id=$game_id");
+    $rowgame= $sql_game_name->fetch_array(MYSQLI_BOTH);
+    $smarty->assign('title_name', $rowgame['game_name']);  
+}
+    
+// Create dropdown values a-z
+$az_value  = az_dropdown_value(0);
+$az_output = az_dropdown_output(0);
+
+$smarty->assign('az_value', $az_value);
+$smarty->assign('az_output', $az_output);
+
+//Send all smarty variables to the templates
+$smarty->display("file:" . $cpanel_template_folder . "menus_disk_title_author.html");
+
+//close the connection
+mysqli_close($mysqli);
