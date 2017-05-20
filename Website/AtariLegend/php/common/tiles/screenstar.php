a197f168fd5132b42b7ced3d3164615ff159e714
stgraveyard <martens_maarten@hotmail.com>
2017-05-20 17:16:18 +0200

    Front page + cpanel
    **MAIN WEBSITE**
    
    _**Frontpage**_
    - The screenstar tile now contains a part of a game review and the layout is updated
    - New updated layout of the main slider at the front screen
    
    **CPANEL**
    
    _**Review section**_ 
    - a 'screenstar' button is added to mark the text which is displayed in the screenstar tile used on the website
    
    _**News section**_ 
    - Font awesome used on approve and edit new pages

--- Website/AtariLegend/php/common/tiles/screenstar.php
+++ Website/AtariLegend/php/common/tiles/screenstar.php
@@ -19,32 +19,46 @@
 $query_screenstar = $mysqli->query("SELECT 
 					game.game_name,
 					game.game_id,
-					comments.comment,
-                    comments.timestamp,
-                    game_user_comments.game_user_comments_id,
+					review_main.review_text,
+                    review_main.review_date,
+                    review_main.review_id,
                     users.user_id,
                     users.userid,
+                    pub_dev.pub_dev_name,
+                    game_year.game_year,
 					screenshot_main.screenshot_id,
 					screenshot_main.imgext
-					FROM game_user_comments
-					LEFT JOIN comments ON (game_user_comments.comment_id = comments.comments_id)
-					LEFT JOIN game ON (game_user_comments.game_id = game.game_id)
-                    LEFT JOIN users ON (comments.user_id = users.user_id)
+					FROM review_game
+					LEFT JOIN review_main ON (review_game.review_id = review_main.review_id)
+					LEFT JOIN game ON (review_game.game_id = game.game_id)
+                    LEFT JOIN users ON (review_main.user_id = users.user_id)
 					LEFT JOIN screenshot_game ON (game.game_id = screenshot_game.game_id)
 					LEFT JOIN screenshot_main ON (screenshot_game.screenshot_id = screenshot_main.screenshot_id)
-					WHERE CHAR_LENGTH( game_name ) <15 AND
-					CHAR_LENGTH( comment ) > 224 AND
-					CHAR_LENGTH( comment ) < 451 ORDER BY RAND() LIMIT 1") or die("query error, screenstar");
+                    LEFT JOIN game_developer ON (game_developer.game_id = game.game_id)
+                    LEFT JOIN pub_dev ON (pub_dev.pub_dev_id = game_developer.dev_pub_id)
+                    LEFT JOIN game_year ON (game_year.game_id = game.game_id)
+					WHERE CHAR_LENGTH( game_name ) <15 ORDER BY RAND() LIMIT 1") or die("query error, screenstar");
 
 $sql_screenstar = $query_screenstar->fetch_array(MYSQLI_BOTH);
 	
 	//Structure and manipulate the comment text
-	$screenstar_comment = $sql_screenstar['comment'];
-	$screenstar_comment = stripslashes($screenstar_comment);
-	$screenstar_comment = InsertALCode($screenstar_comment); 
-	$screenstar_comment = trim($screenstar_comment);
-	$screenstar_comment = RemoveSmillies($screenstar_comment);
-
+	$screenstar_review = $sql_screenstar['review_text']; 
+    
+    $pos_start = strpos($screenstar_review, '[screenstar]');
+    $pos_start = $pos_start;
+    
+    $pos_end = strpos($screenstar_review, '[/screenstar]');
+    $pos_end = $pos_end;
+    
+    $nr_char = $pos_end - $pos_start;
+    
+    $screenstar_review = substr($screenstar_review, $pos_start, $nr_char);
+      
+	$screenstar_review = stripslashes($screenstar_review);
+	$screenstar_review = InsertALCode($screenstar_review); 
+	$screenstar_review = trim($screenstar_review);
+	$screenstar_review = RemoveSmillies($screenstar_review); 
+    
 	//Ready screenshots path and filename
 	$screenstar_image  = $game_screenshot_path;
 	$screenstar_image .= $sql_screenstar['screenshot_id'];
@@ -53,12 +67,14 @@ $sql_screenstar = $query_screenstar->fetch_array(MYSQLI_BOTH);
     	
 	$smarty->assign('screenstar',
 	     array('screenstar_game_name' => $sql_screenstar['game_name'],
-		   'screenstar_comment' => $screenstar_comment,
-           'screenstar_comment_id' => $sql_screenstar['game_user_comments_id'],
+		   'screenstar_review' => $screenstar_review,
+           'screenstar_review_id' => $sql_screenstar['review_id'],
            'screenstar_user_id' => $sql_screenstar['user_id'],
            'screenstar_username' => $sql_screenstar['userid'],
 		   'screenstar_game_id' => $sql_screenstar['game_id'],
-           'screenstar_date'  => date("d/m/Y", $sql_screenstar['timestamp']),
+           'screenstar_date'  => date("d/m/Y", $sql_screenstar['review_date']),
+           'screenstar_developer' => $sql_screenstar['pub_dev_name'],
+           'screenstar_year' => $sql_screenstar['game_year'],   
 		   'screenstar_img' => $screenstar_image));
 
 ?>
