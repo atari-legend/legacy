277fdc7da2d2c5cfcdbff885420ceee6b22777cf
stgraveyard <martens_maarten@hotmail.com>
2017-08-19 00:55:46 +0200

    Games detail page
    - Reviews
    - Responsiveness

--- Website/AtariLegend/php/main/games/games_main_detail.php
+++ Website/AtariLegend/php/main/games/games_main_detail.php
@@ -419,6 +419,119 @@ while ($query_comment = $sql_comment->fetch_array(MYSQLI_BOTH))
 }
 
 
+//***********************************************************************************
+//Get the reviews
+//***********************************************************************************
+if (isset($mode) and $mode == 'review')
+{
+    $sql_review = $mysqli->query("SELECT * FROM game
+                           LEFT JOIN review_game ON (game.game_id = review_game.game_id)
+                           LEFT JOIN review_main ON (review_game.review_id = review_main.review_id)
+                           LEFT JOIN review_score ON (review_main.review_id = review_score.review_id)
+                           LEFT JOIN users ON (review_main.user_id = users.user_id)
+                           WHERE game.game_id = '$game_id' AND review_game.review_id = '$review_id'
+                           AND review_main.review_edit = '0'") or die("Error - Couldn't query review data");
+
+    $query_review = $sql_review->fetch_array(MYSQLI_BOTH);
+
+    $review_date = date("F j, Y", $query_review['review_date']);
+    $review_text = $query_review['review_text'];
+    $review_text = nl2br($review_text);
+    $review_text = InsertALCode($review_text);
+
+    $smarty->assign('review', array(
+        'user_name' => $query_review['userid'],
+        'user_id' => $query_review['user_id'],
+        'email' => $query_review['email'],
+        'game_id' => $query_review['game_id'],
+        'date' => $review_date,
+        'game_name' => $query_review['game_name'],
+        'text' => $review_text
+    ));
+
+    $smarty->assign('score', array(
+        'graphics' => $query_review['review_graphics'],
+        'sound' => $query_review['review_sound'],
+        'gameplay' => $query_review['review_gameplay'],
+        'overall' => $query_review['review_overall']
+    ));
+
+    //Get the screenshots and the comments of this review
+    $query_screenshots = $mysqli->query("SELECT * FROM review_main
+                                    LEFT JOIN screenshot_review ON (review_main.review_id = screenshot_review.review_id)
+                                    LEFT JOIN screenshot_main ON (screenshot_review.screenshot_id = screenshot_main.screenshot_id)
+                                    LEFT JOIN review_comments ON (screenshot_review.screenshot_review_id = review_comments.screenshot_review_id)
+                                    WHERE review_main.review_id = '$review_id' AND review_main.review_edit = '0'");
+
+    while ($sql_screenshots = $query_screenshots->fetch_array(MYSQLI_BOTH)) {
+        $new_path = $game_screenshot_path;
+        $new_path .= $sql_screenshots['screenshot_id'];
+        $new_path .= ".";
+        $new_path .= $sql_screenshots['imgext'];
+
+        $smarty->append('screenshots_review', array(
+            'screenshot' => $new_path,
+            'comment' => $sql_screenshots['comment_text']
+        ));
+        $count++;
+    }  
+
+    $smarty->assign("mode", 'review');        
+}    
+else
+{    
+    $sql_review = $mysqli->query("SELECT * FROM game
+                               LEFT JOIN review_game ON (game.game_id = review_game.game_id)
+                               LEFT JOIN review_main ON (review_game.review_id = review_main.review_id)
+                               LEFT JOIN review_score ON (review_main.review_id = review_score.review_id)
+                               LEFT JOIN users ON (review_main.user_id = users.user_id)
+                               WHERE game.game_id = '$game_id' AND review_main.review_edit = '0'") or die("Error - Couldn't query review data");
+
+    while ($query_review = $sql_review->fetch_array(MYSQLI_BOTH)) 
+    {
+        $review_date = date("F j, Y", $query_review['review_date']);
+        
+        //Structure and manipulate the review text
+        $review_text = $query_review['review_text'];
+        
+        $pos_start = strpos($review_text , '[frontpage]');  
+        $pos_end = strpos($review_text , '[/frontpage]');    
+        $nr_char = $pos_end - $pos_start;
+        
+        $review_text  = substr($review_text , $pos_start, $nr_char);  
+        $review_text = nl2br($review_text);
+        $review_text = InsertALCode($review_text); // disabled this as it wrecked the design.
+        $review_text = trim($review_text);
+        $review_text = RemoveSmillies($review_text);
+
+        //Get a screenshots and the comments of this review
+        $query_screenshots_review = $mysqli->query("SELECT * FROM review_main
+                                        LEFT JOIN screenshot_review ON (review_main.review_id = screenshot_review.review_id)
+                                        LEFT JOIN screenshot_main ON (screenshot_review.screenshot_id = screenshot_main.screenshot_id)
+                                        LEFT JOIN review_comments ON (screenshot_review.screenshot_review_id = review_comments.screenshot_review_id)
+                                        WHERE review_main.review_id = '$query_review[review_id]' AND review_main.review_edit = '0' ORDER BY RAND() LIMIT 1") or die("Error - Couldn't query review screenshots");
+        
+        $sql_screenshots_review = $query_screenshots_review->fetch_array(MYSQLI_BOTH);
+        
+        $new_path = $game_screenshot_path;
+        $new_path .= $sql_screenshots_review['screenshot_id'];
+        $new_path .= ".";
+        $new_path .= $sql_screenshots_review['imgext'];
+        
+        $smarty->append('review', array(
+                'user_name' => $query_review['userid'],
+                'user_id' => $query_review['user_id'],
+                'review_id' => $query_review['review_id'],
+                'email' => $query_review['email'],
+                'game_id' => $query_review['game_id'],
+                'date' => $review_date,
+                'game_name' => $query_review['game_name'],
+                'text' => $review_text,
+                'screenshot' => $new_path,
+                'comment' => $sql_screenshots_review['comment_text']
+            ));
+    }
+}
 
 $smarty->assign("game_id", $game_id);
 
