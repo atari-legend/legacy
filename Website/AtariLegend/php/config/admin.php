f520aee76884f00a39cea9884d65e1b4a1b6ddb0
stgraveyard <martens_maarten@hotmail.com>
2017-09-18 21:11:48 +0200

    Main site
    - Social corner tile
    - deactivate tos and picasso skin for now
    - Partly HTML validation of final pages
    - checking all pages and adding/modifying tiles
    - fixing all links
    - removal of final bugs

--- Website/AtariLegend/php/config/admin.php
+++ Website/AtariLegend/php/config/admin.php
@@ -11,6 +11,13 @@
 //Include this file if you want a page to require admin permission level
 //Needs to be included after common.php is included.
 
+
+//this include is a shortcut. I wanted to visitors also to be able to do bugreports at cpanel level.
+//But I did not want to include this file in every single page, so I placed the include here.
+include("../../common/tiles/tile_bug_report.php");
+
+
+//This is the actual authorization check
 if (login_check($mysqli) == false) 
 {    
     $_SESSION['edit_message'] = "Please log in to use this functionality";
