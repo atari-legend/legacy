73fc4500f07db19b1c46469e764d1c9b010eb0ea
Unknown <silversurfer@atari-forum.com>
2017-10-10 23:21:17 +0200

    Fix indentation
    minor fixes

--- Website/AtariLegend/php/main/interviews/interviews_main.php
+++ Website/AtariLegend/php/main/interviews/interviews_main.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *                                interviews_main.php
+*                                interviews_main.php
 *                            ------------------------------
 *   begin                : Monday, August 21, 2017
 *   copyright            : (C) 2017 Atari Legend
@@ -10,7 +10,7 @@
 ****************************************************************************/
 
 //****************************************************************************************
-// This is the main page of the interviews. 
+// This is the main page of the interviews.
 //****************************************************************************************
 
 //load all common functions
@@ -33,15 +33,14 @@ $v_rows = $query_number->num_rows;
 
 //main query
 $sql_interview = $mysqli->query("SELECT *
-                        FROM interview_main
-                        LEFT JOIN interview_text on (interview_main.interview_id = interview_text.interview_id)
-                        LEFT JOIN users on (interview_main.user_id = users.user_id)
-                        LEFT JOIN individuals on (interview_main.ind_id = individuals.ind_id)
-                        LEFT JOIN individual_text on (interview_main.ind_id = individual_text.ind_id)
-                        ORDER BY interview_text.interview_date DESC LIMIT  " . $v_counter . ", 5") or die("Error - Couldn't query interview data");
+                       FROM interview_main
+                       LEFT JOIN interview_text on (interview_main.interview_id = interview_text.interview_id)
+                       LEFT JOIN users on (interview_main.user_id = users.user_id)
+                       LEFT JOIN individuals on (interview_main.ind_id = individuals.ind_id)
+                       LEFT JOIN individual_text on (interview_main.ind_id = individual_text.ind_id)
+                       ORDER BY interview_text.interview_date DESC LIMIT  " . $v_counter . ", 5") or die("Error - Couldn't query interview data");
 
-while ($query_interview = $sql_interview->fetch_array(MYSQLI_BOTH))
-{	  
+while ($query_interview = $sql_interview->fetch_array(MYSQLI_BOTH)) {
     $v_interview_date = date("F j, Y", $query_interview['interview_date']);
 
     $interview_text = $query_interview['interview_intro'];
@@ -52,69 +51,70 @@ while ($query_interview = $sql_interview->fetch_array(MYSQLI_BOTH))
     $interview_chapters = nl2br($interview_chapters);
     $interview_chapters = InsertALCode($interview_chapters);
 
-    //get the profile of the author
-    if(preg_match("/[a-z]/i", $query_interview['ind_profile'])){
-        $profile = $query_interview['ind_profile'];
-    }
-    else {$profile = 'none';}
+   //get the profile of the author
+   if (preg_match("/[a-z]/i", $query_interview['ind_profile'])) {
+       $profile = $query_interview['ind_profile'];
+   } else {
+       $profile = 'none';
+   }
 
-    //The interviewed person's picture
-    if ($query_interview['ind_imgext'] == 'png' or $query_interview['ind_imgext'] == 'jpg' or $query_interview['ind_imgext'] == 'gif') {
-        $v_ind_image = $individual_screenshot_path;
-        $v_ind_image .= $query_interview['ind_id'];
-        $v_ind_image .= '.';
-        $v_ind_image .= $query_interview['ind_imgext'];
-    } else {
-        $v_ind_image = "none";
-    }
+   //The interviewed person's picture
+   if ($query_interview['ind_imgext'] == 'png' or $query_interview['ind_imgext'] == 'jpg' or $query_interview['ind_imgext'] == 'gif') {
+       $v_ind_image = $individual_screenshot_path;
+       $v_ind_image .= $query_interview['ind_id'];
+       $v_ind_image .= '.';
+       $v_ind_image .= $query_interview['ind_imgext'];
+   } else {
+       $v_ind_image = "none";
+   }
 
-    $smarty->append('interviews', array(
-        'individual_name' => $query_interview['ind_name'],
-        'individual_id' => $query_interview['ind_id'],
-        'individual_profile' => $profile,
-        'interview_author' => $query_interview['userid'],
-        'interview_author_id' => $query_interview['user_id'],
-        'interview_id' => $query_interview['interview_id'],
-        'email' => $query_interview['email'],
-        'user_fb' => $query_interview['user_fb'],
-        'user_website' => $query_interview['user_website'],
-        'user_twitter' => $query_interview['user_twitter'],
-        'user_af' => $query_interview['user_af'],
-        'interview_date' => $v_interview_date,
-        'interview_img' => $v_ind_image,
-        'interview_intro' => $interview_text
-    ));
+   $smarty->append('interviews', array(
+       'individual_name' => $query_interview['ind_name'],
+       'individual_id' => $query_interview['ind_id'],
+       'individual_profile' => $profile,
+       'interview_author' => $query_interview['userid'],
+       'interview_author_id' => $query_interview['user_id'],
+       'interview_id' => $query_interview['interview_id'],
+       'email' => $query_interview['email'],
+       'user_fb' => $query_interview['user_fb'],
+       'user_website' => $query_interview['user_website'],
+       'user_twitter' => $query_interview['user_twitter'],
+       'user_af' => $query_interview['user_af'],
+       'interview_date' => $v_interview_date,
+       'interview_img' => $v_ind_image,
+       'interview_intro' => $interview_text
+   ));
 }
 
-$smarty->assign('nr_interviews',$v_rows);
+$smarty->assign('nr_interviews', $v_rows);
 
 //Check if back arrow is needed
 if ($v_counter > 0) {
-    // Build the link
-    $v_linkback = ('?v_counter=' . ($v_counter - 5));
+   // Build the link
+   $v_linkback = ('?v_counter=' . ($v_counter - 5));
 }
 
 //Check if we need to place a next arrow
 if ($v_rows > ($v_counter + 5)) {
-    //Build the link
-    $v_linknext = ('?v_counter=' . ($v_counter + 5));
+   //Build the link
+   $v_linknext = ('?v_counter=' . ($v_counter + 5));
 }
 
 if (empty($c_counter)) {
-    $c_counter = "";
+   $c_counter = "";
 }
 if (empty($v_linkback)) {
-    $v_linkback = "";
+   $v_linkback = "";
 }
 if (empty($v_linknext)) {
-    $v_linknext = "";
+   $v_linknext = "";
 }
 
 $smarty->assign('links', array(
-    'linkback' => $v_linkback,
-    'linknext' => $v_linknext,
-    'v_counter' => $v_counter,
-    'c_counter' => $c_counter
+   'linkback' => $v_linkback,
+   'linknext' => $v_linknext,
+   'v_counter' => $v_counter,
+   'c_counter' => $c_counter
 ));
 
 
