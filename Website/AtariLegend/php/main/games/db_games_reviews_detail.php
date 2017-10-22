64165552a253e4ab3bff928717c5bf67fd5fc754
stgraveyard <martens_maarten@hotmail.com>
2017-10-22 11:39:36 +0200

    Fix for issue #306 ?
    Added some extra checks in the DB files of the main site.

--- Website/AtariLegend/php/main/games/db_games_reviews_detail.php
+++ Website/AtariLegend/php/main/games/db_games_reviews_detail.php
@@ -15,6 +15,7 @@
 //*********************************************************************************************
 
 include("../../config/common.php");
+include("../../config/admin.php");
 
 if (isset($action)) {
     if ($action=='delete_comment') {
