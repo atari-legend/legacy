fd0278fe131ac01c9a6d8092a445122e781f71fd
Nicolas Guillaumin <nicolas+github@guillaumin.me>
2017-11-08 18:52:51 -0800

    Generate sitemaps for Google to index all of our content
    Google seems to be missing some content that we offer, for example as of
    yesterday it had only crawled 750 pages where it should have been a lot
    more (since we have a lot more games that each have their individual
    page).
    
    To assist Google, and following their recommendations, generate
    [Sitemaps](https://support.google.com/webmasters/answer/156184?hl=en)
    that list all the content that need to be indexed. Where it makes sense,
    also give a hint of how often the content is updated so that Google can
    re-fetch it on the right schedule (rather than hammering us every day
    with crawl requests).
    
    So:
    * Added a `robots.txt` to give the location of the sitemap. Also prevent
    `/admin/` to be indexed, it shouldn't happen as it require
    authentication but it doesn't hurt to specify it
    * Generate a "sitemap index" to break down our sitemaps in multiple
    files
    * Generate a general sitemap containing some of the pages (e.g. News) as
    well as all the interviews, and reviews page
    * Generate one sitemap file per game, one for each letter of the
    alphabet, 0-9 and special characters
    
    I added a new constant `REQUEST_SITEURL` containing the actual URL used
    to access the site (e.g. `localhost` on development, `dev.stonish.net`
    on dev, etc.) to be able to test things: Sitemaps must contain absolute
    URLs, and I couldn't test it if I hardcoded it to www.atarilegend.com.

---
+++ Website/AtariLegend/php/main/front/sitemap_games.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Generate a sitemap for games starting with a given letter
+ *
+ * This generates a sitemap providing links to each individual games starting
+ * with a specific letter or number
+ *
+ * In addition, the special value '-' generates a list of all game that start
+ * with anything else than a letter or number (e.g. 'EE's Lost His Marbles)
+ */
+
+require("../../config/common.php");
+
+// Letter parameter is mandatory, and must only have one character
+// which is a letter, number or '-'
+if (! isset($letter) || !preg_match("/^[a-z0-9-]$/i", $letter)) {
+	http_response_code(400);
+	exit();
+}
+
+if ($letter == "-") {
+	// Special case: Select all games that don't start with a letter or number
+	$stmt = $mysqli->prepare("SELECT game_id FROM game WHERE LOWER(game_name) NOT REGEXP('^[a-zA-Z0-9]') ORDER BY game_id")
+		or die($mysqli->error);
+} else {
+	// Select all games stating with the given letter or number
+	$stmt = $mysqli->prepare("SELECT game_id FROM game WHERE LOWER(game_name) LIKE CONCAT(LOWER(?), '%') ORDER BY game_id")
+		or die($mysqli->error);
+	$stmt->bind_param("s", $letter)
+		or die($mysqli->error);
+}
+
+$stmt->execute() or die($mysqli->error);
+$stmt->bind_result($game_id) or die($mysqli->error);
+
+// Fetch all game ids for the letter
+$game_ids = [];
+while ($stmt->fetch()) {
+	$game_ids[] = $game_id;
+}
+
+$stmt->close();
+
+header("Content-Type: application/xml");
+
+$smarty->assign("game_ids", $game_ids);
+$smarty->display("file:${mainsite_template_folder}sitemap_games.xml");
