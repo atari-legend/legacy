416e25b0c79f06f231fb941dfba85ad322f12163
Nicolas Guillaumin <nicolas+github@guillaumin.me>
2018-01-06 14:38:48 -0800

    Updated purge script to also purge media / images dump
    The script only purged SQL dumps, but we also want to cleanup image
    ZIPs, so make it a bit more generic to purge any file that have a name
    starting with a date.

--- Website/AtariLegend/php/admin/administration/purge_db_dumps.php
+++ Website/AtariLegend/php/admin/administration/purge_db_dumps.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Script to purge old database dumps (backups and public dumps)
+ * Script to purge old database and media dumps (backups and public dumps)
  *
  * Takes one argument: The path to the folder containing the dumps to purge.
  *
- * Dumps are expected to be named with a date, e.g. 2017-01-01.sql
+ * Dumps are expected to be named with a date, e.g. 2017-01-01<something>
  */
 
 // Interval of backups to keep. See the PHP DateInterval class for the syntax
@@ -17,8 +17,8 @@ if (php_sapi_name() !== "cli") {
 
 // Check presence of file path in arguments
 if (sizeof($argv) < 2) {
-    echo "Usage: ".basename(__FILE__)." </path/to/database/dumps>\n\n";
-    echo "  Purge database dumps older than an interval (".PURGE_INTERVAL.")\n";
+    echo "Usage: ".basename(__FILE__)." </path/to/dumps>\n\n";
+    echo "  Purge DB or media dumps older than an interval (".PURGE_INTERVAL.")\n";
     exit(1);
 }
 
@@ -27,18 +27,18 @@ $path = $argv[1];
 $oldest_date = new DateTime();
 $oldest_date->sub(new DateInterval(PURGE_INTERVAL));
 
-echo "Backups older than ".$oldest_date->format("Y-m-d")." will be purged.\n\n";
+echo "Dumps older than ".$oldest_date->format("Y-m-d")." will be purged.\n\n";
 
 foreach (scandir($path) as $file) {
-    if (preg_match("/\d{4}-\d{2}-\d{2}\.sql\.gz/", $file, $matches)) {
-        $filedate = new DateTime(basename($file, ".sql.gz"));
+    if (preg_match("/^(\d{4}-\d{2}-\d{2})/", $file, $matches)) {
+        $filedate = new DateTime($matches[1]);
         $filepath = $path."/".$file;
 
         if ($filedate < $oldest_date) {
-            echo "Removing old backup: $filepath\n";
-            unlink($filepath) or die("Error deleting backup $filepath");
+            echo "Removing old dump: $filepath\n";
+            unlink($filepath) or die("Error deleting dump $filepath");
         } else {
-            echo "Keeping recent backup: $filepath\n";
+            echo "Keeping recent dump: $filepath\n";
         }
     }
 }
