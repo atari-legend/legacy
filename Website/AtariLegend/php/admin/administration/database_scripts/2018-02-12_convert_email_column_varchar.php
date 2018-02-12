6dc57e8bb04165d44135478411fe74752e6cb63f
Unknown <silversurfer@atari-forum.com>
2018-02-12 21:32:10 +0100

    Fix for temporary tables
    - Apparently text and blob tables are not compatible with temporary tables... which was a huge surprise to me. No real reason for the email table to be a text anyway I think.

---
+++ Website/AtariLegend/php/admin/administration/database_scripts/2018-02-12_convert_email_column_varchar.php
@@ -0,0 +1,41 @@
+<?php
+/***************************************************************************
+*                                2018-02-12_convert_email_column_varchar.php
+*                            ----------------------------------------------
+*   begin                : 2018-02-12
+*   copyright            : (C) 2018 Atari Legend
+*   email                : silversurfer@atari-forum.com
+*   actual update        :
+*
+*
+***************************************************************************/
+
+// Unique identifier set by developer.
+$database_update_id = 110;
+
+// Description of what the change will do.
+$update_description = "Convert user email to varchar";
+
+// Should the database change query execute if test is "test_fail" or "test_success"
+$execute_condition = "test_success";
+
+//This is the test query, the query should be made to get an either true or false result.
+$test_condition = "SELECT *
+FROM information_schema.columns
+WHERE table_schema = '$db_databasename'
+AND table_name = 'users'
+AND column_name = 'email'
+AND column_type = 'text' LIMIT 1";
+
+// Database change
+$database_update_sql = "
+ALTER TABLE `users`
+CHANGE `email` `email` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;";
+
+// If the update should auto execute without user interaction set to "yes".
+$database_autoexecute = "yes";
+
+// This var should almost allways be set to "no", it is only used for certain corner cases where
+// the database change has already been done in some other way and we only want to update the
+// change database.
+$force_insert = "no";
