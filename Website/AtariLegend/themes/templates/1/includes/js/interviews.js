d5e6446ed35cb4de22073e2115d85326f3a135ce
stgraveyard <martens_maarten@hotmail.com>
2018-02-20 16:46:21 +0100

    Adding the new buttons and some final cleanup

--- Website/AtariLegend/themes/templates/1/includes/js/interviews.js
+++ Website/AtariLegend/themes/templates/1/includes/js/interviews.js
@@ -4,9 +4,52 @@
  *
  */
 
+window.onload = function () {
+    $('.interview_main_text').dotdotdot({ // This is the script to truncate the preview of the reviews
+        //  configuration goes here
+
+        /*  The text to add as ellipsis. */
+        ellipsis: '... ',
+
+        /*  How to cut off the text/html: 'word'/'letter'/'children' */
+        wrap: 'word',
+
+        /*  Wrap-option fallback to 'letter' for long words */
+        fallbackToLetter: false,
+
+        /*  jQuery-selector for the element to keep and put after the ellipsis. */
+        after: null,
+
+        /*  Whether to update the ellipsis: true/'window' */
+        watch: window,
+
+        /*  Optionally set a max-height, can be a number or function.
+            If null, the height will be measured. */
+        height: null,
+
+        /*  Deviation for the height-option. */
+        tolerance: 0,
+
+        /*  Callback function that is fired after the ellipsis is added,
+            receives two parameters: isTruncated(boolean), orgContent(string). */
+        callback: function (isTruncated, orgContent) {},
+
+        lastCharacter: {
+
+            /*  Remove these characters from the end of the truncated text. */
+            remove: [ ' ', ',', ';', '.', '!', '?' ],
+
+            /*  Don't add an ellipsis if this array contains
+                the last character of the truncated text. */
+            noEllipsis: []
+        }
+    });
+};
+
 $(document).ready(function () {
     $('select[name=members]').altAutocomplete();
     $('select[name=individual]').altAutocomplete();
+    $('select[name=individual_create]').altAutocomplete();
 });
 
 window.openTab = function (evt, tabName, screenshotsNr) {
