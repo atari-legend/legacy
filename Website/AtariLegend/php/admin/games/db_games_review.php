f3df018d6872b27e00d95393c9be8bae6863fe24
stgraveyard <martens_maarten@hotmail.com>
2018-02-21 17:20:35 +0100

    Completely updating the game review section

--- Website/AtariLegend/php/admin/games/db_games_review.php
+++ Website/AtariLegend/php/admin/games/db_games_review.php
@@ -30,10 +30,13 @@ include("../../config/admin_rights.php");
 
 if (isset($action) and $action == 'add_review') {
     // first we have to convert the date vars into a time stamp to be inserted to review_date
-    $date = date_to_timestamp($Date_Year, $Date_Month, $Date_Day);
-    $textfield = $mysqli->real_escape_string($textfield);
-
-    $sdbquery = $mysqli->query("INSERT INTO review_main (user_id, review_text, review_date) VALUES ($members, '$textfield', '$date')") or die("Couldn't insert into review_main");
+    $date_year = date("Y");
+    $date_month = date("m");
+    $date_day = date("d");
+    
+    $date = date_to_timestamp($date_year, $date_month, $date_day);
+    
+    $sdbquery = $mysqli->query("INSERT INTO review_main (review_date) VALUES ('$date')") or die("Couldn't insert into review_main");
 
     //get the id of the inserted review
     $REVIEW = $mysqli->query("SELECT review_id FROM review_main
@@ -44,47 +47,11 @@ if (isset($action) and $action == 'add_review') {
     $reviewid = $reviewrow[0];
 
     //Then, we'll be filling up the game review table
-    $sdbquery = $mysqli->query("INSERT INTO review_game (review_id, game_id) VALUES ($reviewid, $game_id)") or die("Couldn't insert into review_game");
-
-    //Fill the score table
-    if ($graphics == '' or $sound == '' or $gameplay == '' or $conclusion == '') {
-    } else {
-        $sdbquery = $mysqli->query("INSERT INTO review_score (review_id, review_graphics, review_sound, review_gameplay, review_overall) VALUES ($reviewid, $graphics, $sound, $gameplay, $conclusion)") or die("Couldn't insert into review_score");
-    }
-
-    //we're gonna add the screenhots into the screenshot_review table and fill up the review_comment table.
-    //We need to loop on the screenshot table to check the shots used. If a comment field is filled,
-    //the screenshot was used!
-    $SCREEN = $mysqli->query("SELECT * FROM screenshot_game where game_id = '$game_id' ORDER BY screenshot_id ASC") or die("Database error - getting screenshots");
-
-    $i = 0;
-
-    while ($screenrow = $SCREEN->fetch_row()) {
-        if ($inputfield[$i] != "") {
-            //fill the review_screenshot table
-
-            $screenid = $screenrow[2];
-            $comment  = $inputfield[$i];
-            $comment = $mysqli->real_escape_string($comment);
-
-            $sdbquery = $mysqli->query("INSERT INTO screenshot_review (review_id, screenshot_id) VALUES ('$reviewid', '$screenid')") or die("Couldn't insert into screenshot_review");
-
-            $REVIEWSHOT = $mysqli->query("SELECT * FROM screenshot_review
-                                           ORDER BY screenshot_review_id desc") or die("Database error - selecting screenshots review");
-
-            $reviewshotrow = $REVIEWSHOT->fetch_row();
-
-            $reviewshotid = $reviewshotrow[0];
-
-            //fill the screenshot comment table
-            $sdbquery = $mysqli->query("INSERT INTO review_comments (screenshot_review_id, comment_text) VALUES ('$reviewshotid', '$comment')") or die("Couldn't insert into review_comments");
-        }
-        $i++;
-    }
+    $sdbquery = $mysqli->query("INSERT INTO review_game (review_id, game_id) VALUES ($reviewid, $game_create)") or die("Couldn't insert into review_game");
 
     create_log_entry('Games', $game_id, 'Review', $reviewid, 'Insert', $_SESSION['user_id']);
 
     $_SESSION['edit_message'] = "Review added to DB";
 
-    header("Location: ../games/games_review_add.php?game_id=$game_id");
+    header("Location: ../games/games_review_edit.php?game_id=$game_create&reviewid=$reviewid");
 }
---
+++ Website/AtariLegend/themes/templates/1/includes/js/game_review.js
@@ -0,0 +1,87 @@
+/*! 
+ * game_review.js 
+ * http://www.atarilegend.com 
+ * 
+ */ 
+
+$(document).ready(function () { 
+    $('select[name=game_create]').altAutocomplete(); 
+    $('select[name=members]').altAutocomplete();
+}); 
+
+function deletereview(reviewid, gameid) {
+    $("#JSGenericModal").dialog({
+        title: "Delete",
+        open: $("#JSGenericModalText").text("Are you sure you want to delete this review?"),
+        resizable: false,
+        height: 200,
+        modal: true,
+        buttons: {
+            "Delete": function() {
+                $(this).dialog("close");
+                url="db_games_review_edit.php?reviewid="+reviewid+"&game_id="+gameid+"&action=delete_review";
+                location.href=url;
+            },
+            Cancel: function() {
+                $(this).dialog("close");
+            }
+        }
+    });
+}
+function deleteSubmission(reviewid, gameid) {
+    $("#JSGenericModal").dialog({
+        title: "Delete",
+        open: $("#JSGenericModalText").text("Are you sure you want to delete this review?"),
+        resizable: false,
+        height: 200,
+        modal: true,
+        buttons: {
+            "Delete": function() {
+                $(this).dialog("close");
+                url="db_games_review_edit.php?reviewid="+reviewid+"&game_id="+gameid+"&action=delete_submission";
+                location.href=url;
+            },
+            Cancel: function() {
+                $(this).dialog("close");
+            }
+        }
+    });
+}
+function move_to_comment(reviewid, gameid) {
+    $("#JSGenericModal").dialog({
+        title: "Move",
+        open: $("#JSGenericModalText").text("Are you sure you want to move this review to the comments section? Some content will be deleted in the moving process."),
+        resizable: false,
+        height: 200,
+        modal: true,
+        buttons: {
+            "Move": function() {
+                $(this).dialog("close");
+                url="db_games_review_edit.php?reviewid="+reviewid+"&game_id="+gameid+"&action=move_to_comment";
+                location.href=url;
+            },
+            Cancel: function() {
+                $(this).dialog("close");
+            }
+        }
+    });
+}
+function deletecomment(game_id,reviewid,screenshot_id) {
+    $("#JSGenericModal").dialog({
+        title: "Delete",
+        open: $("#JSGenericModalText").text("Are you sure you want to delete this comment?"),
+        resizable: false,
+        height: 200,
+        modal: true,
+        buttons: {
+            "Delete": function() {
+                $(this).dialog("close");
+                url="db_games_review_edit.php?game_id="+game_id+"&reviewid="+reviewid+"&screenshot_id="+screenshot_id+"&action=delete_comment";
+                location.href=url;
+            },
+            Cancel: function() {
+                $(this).dialog("close");
+            }
+        }
+    });
+}
