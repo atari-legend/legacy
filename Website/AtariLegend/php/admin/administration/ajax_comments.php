80d48dc9c93e60d63eead96177684495b355c356
Unknown <silversurfer@atari-forum.com>
2018-02-25 19:29:44 +0100

    Initial cpanel comments
    Design just about ready
    Cpanel comments tweak the php
    
    - merging all comments into one, needed temporary tables for that.
    Fix for temporary tables
    
    - Apparently text and blob tables are not compatible with temporary tables... which was a huge surprise to me. No real reason for the email table to be a text anyway I think.
    Ajax up and running
    
    - Ajax implemented on the comments navigator
    - Remove all old comments pages
    - fix a word wrap issue.
    autoload and delete
    
    autoload on scroll implemented
    delete function reimplimented
    indentation
    
    fixed indentation
    DAO stuck
    
    So, not going that well atm...
    DAO done
    
    finally got it right
    More functionality
    
    edit function
    link to submissions
    comment box buttons responsive
    Cpanel comments
    
    Separated the js into its own file
    Display to which section each comment belong to,  only for user and all list.
    marty fixes
    
    FIxes various issues in comments
    changelog for edit
    
    changelog for edit and various other minor glitches fixed.

---
+++ Website/AtariLegend/php/admin/administration/ajax_comments.php
@@ -0,0 +1,54 @@
+<?php
+/***************************************************************************
+ *                                ajax_comments.php
+ *                            -----------------------
+ *   begin                : 2018-02-12
+ *   copyright            : (C) 2018 Atari Legend
+ *   email                : silversurfer@atari-forum.com
+ *   actual update        : file creation
+ *
+ *
+ ***************************************************************************/
+
+include("../../config/common.php");
+include("../../config/admin.php");
+require_once __DIR__."/../../lib/Db.php";
+require_once __DIR__."/../../common/DAO/CommentsDAO.php";
+
+$commentsDao = new AL\Common\DAO\CommentsDAO($mysqli);
+
+//*********************************************************************************************
+// User comments
+//*********************************************************************************************
+
+if (isset($view) and $view == "users_comments") {
+    $view = "users_comments";
+} elseif (isset($view) and $view == "comments_game_comments") {
+    $view = "comments_game_comments";
+} elseif (isset($view) and $view == "comments_all") {
+    $view = "comments_all";
+} elseif (isset($view) and $view == "comments_game_review_comments") {
+    $view = "comments_game_review_comments";
+} elseif (isset($view) and $view == "comments_interview_comments") {
+    $view = "comments_interview_comments";
+} else {
+    $view = "latest_comments";
+}
+
+$smarty->assign(
+    'comments',
+    $commentsDao->getCommentsBuild(
+        isset($view) ? $view : null,
+        isset($user_id) ? $user_id : null,
+        isset($action) ? $action : null,
+        isset($last_timestamp) ? $last_timestamp : null
+    )
+);
+
+$smarty->assign('links', array(
+    'view' => $view,
+    'getCommentCount' => $commentsDao->getCommentCount(),
+));
+
+//Send all smarty variables to the templates
+$smarty->display("file:" . $cpanel_template_folder . "ajax_comments.html");
