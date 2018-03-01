4904ea9106167fc5bd59774035deb58be8e04b1b
stgraveyard <martens_maarten@hotmail.com>
2018-03-01 17:52:53 +0100

    Fixes #1
    - added user comments in left nav
    - Fixed BBCODE in comments section
    - Fixed links to review edit page when dealing with a review comment
    - removed some abundant files from the review section
    - updating the changelog --> Still need to test this thouroughly

--- Website/AtariLegend/php/admin/administration/change_log.php
+++ Website/AtariLegend/php/admin/administration/change_log.php
@@ -107,13 +107,15 @@ while ($log = $sql_log->fetch_array(MYSQLI_BOTH)) {
             if ($log['action'] == 'Delete') {
                 $subsection_link = ("../administration/change_log.php");
             } else {
-                $subsection_link = ("../games/games_comment_edit.php" . '?game_user_comments_id=' . $log['sub_section_id'] . '&v_counter=0');
+                //$subsection_link = ("../games/games_comment_edit.php" . '?game_user_comments_id=' . $log['sub_section_id'] . '&v_counter=0');
+                $subsection_link = ("../administration/comments.php");
             }
         }
 
         if ($log['sub_section'] == 'Review' or $log['sub_section'] == 'Review comment') {
             if ($log['action'] == 'Delete') {
-                $subsection_link = ("../games/games_review_add.php" . '?game_id=' . $log['section_id']);
+                //$subsection_link = ("../games/games_review_add.php" . '?game_id=' . $log['section_id']);
+                $subsection_link = ("../games/games_review.php");
             } else {
                 $subsection_link = ("../games/games_review_edit.php" . '?reviewid=' . $log['sub_section_id'] . '&game_id=' . $log['section_id']);
             }
@@ -242,7 +244,8 @@ while ($log = $sql_log->fetch_array(MYSQLI_BOTH)) {
             if ($log['action'] == 'Delete') {
                 $subsection_link = ("../administration/change_log.php");
             } else {
-                $subsection_link = ("../interviews/interviews_comment_edit.php" . '?interview_user_comments_id=' . $log['sub_section_id'] . '&v_counter=0');
+                //$subsection_link = ("../interviews/interviews_comment_edit.php" . '?interview_user_comments_id=' . $log['sub_section_id'] . '&v_counter=0');
+                $subsection_link = ("../administration/comments.php");
             }
         }
     }
@@ -255,7 +258,8 @@ while ($log = $sql_log->fetch_array(MYSQLI_BOTH)) {
             if ($log['action'] == 'Delete') {
                 $subsection_link = ("../administration/change_log.php");
             } else {
-                $subsection_link = ("../games/games_review_comment_edit.php" . '?review_user_comments_id=' . $log['sub_section_id'] . '&v_counter=0');
+                //$subsection_link = ("../games/games_review_comment_edit.php" . '?review_user_comments_id=' . $log['sub_section_id'] . '&v_counter=0');
+                $subsection_link = ("../administration/comments.php");
             }
         }
     }
