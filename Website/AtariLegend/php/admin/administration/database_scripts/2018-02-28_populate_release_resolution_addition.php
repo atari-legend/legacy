9dfc78eb82a102ad0383c2d5434fb4b2d7e72baf
stgraveyard <martens_maarten@hotmail.com>
2018-06-13 07:52:08 +0200

    Removing unnecesarry files
    Clean up of old tables

--- Website/AtariLegend/php/admin/administration/database_scripts/2018-02-28_populate_release_resolution_addition.php
+++ Website/AtariLegend/php/admin/administration/database_scripts/2018-02-28_populate_release_resolution_addition.php
@@ -7,38 +7,27 @@
 // Lets do mono First
 //
 
-// first fix bug
-$mysqli->query("
-DELETE FROM game_mono WHERE game_id = 4072")
-    or die("Error selecting game_mono tables: ".$mysqli->error);
-$mysqli->query("
-DELETE FROM game_mono WHERE game_id = 5313")
-    or die("Error selecting game_mono tables: ".$mysqli->error);
-$mysqli->query("
-DELETE FROM game_mono WHERE game_id = 5386")
-    or die("Error selecting game_mono tables: ".$mysqli->error);
-$mysqli->query("
-DELETE FROM game_mono WHERE game_id = 5741")
-    or die("Error selecting game_mono tables: ".$mysqli->error);
+//Let's first loop over the game_mono table
+$result1 = $mysqli->query("SELECT * FROM game_mono")
+                            or die("Unable to retrieve the list of game_mono titles: ".$mysqli->error);
 
-$result3 = $mysqli->query("
-SELECT
-    game_release.id AS game_release_id
-FROM
-    game_mono
-LEFT JOIN game_release ON (game_release.game_id = game_mono.game_id )")
-or die("Unable to retrieve the list of falcon only titles: ".$mysqli->error);
+while ($game_mono = $result1->fetch_array(MYSQLI_BOTH)) {
+    //  Now let's see if we have a game release for this entry. This is not always the case so it seems.
+    $result5 = $mysqli->query("SELECT * FROM game_release WHERE game_id = $game_mono[game_id]")
+                            or die("Unable to retrieve the game_id from the game release table: ".$mysqli->error);
 
-while ($game_mono = $result3->fetch_array(MYSQLI_BOTH)) {
-    $game_release_id = $game_mono['game_release_id'];
+    while ($game_release = $result5->fetch_array(MYSQLI_BOTH)) {
 
-    // Insert mono/ST Highrez
-    $mysqli->query("
-    INSERT INTO game_release_resolution
-        (resolution_id,game_release_id)
-    VALUES
-        (3, $game_release_id)")
-    or die("Error error inserting ST high rez: ".$mysqli->error);
+        $game_release_id = $game_release['id'];
+
+        // Insert mono/ST Highrez
+        $mysqli->query("
+        INSERT INTO game_release_resolution
+            (resolution_id,game_release_id)
+        VALUES
+            (3, $game_release_id)")
+        or die("Error error inserting ST high rez: ".$mysqli->error);
+    }
 }
 
 //
