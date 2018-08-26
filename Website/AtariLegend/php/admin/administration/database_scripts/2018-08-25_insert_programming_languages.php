bd4a76d1ad27f38c7eaf1619f7cf1fc857bcc110
stgraveyard <martens_maarten@hotmail.com>
2018-08-26 23:59:11 +0200

    Creating new tables and deleting old ones
    STOS, STAC and SEUCK have been merged into game_programming_language

---
+++ Website/AtariLegend/php/admin/administration/database_scripts/2018-08-25_insert_programming_languages.php
@@ -0,0 +1,36 @@
+<?php
+/***************************************************************************
+ * Insert programming languages into the programming languages table
+ **************************************************************************/
+
+// Unique identifier set by developer.
+$database_update_id = 178;
+
+// Description of what the change will do.
+$update_description = "Insert programming languages into the programming languages table";
+
+// Should the database change query execute if test is "test_fail" or "test_success"
+$execute_condition = "test_success";
+
+//This is the test query, the query should be made to get an either true or false result.
+$test_condition = "SELECT *
+FROM information_schema.columns
+WHERE table_schema = '$db_databasename'
+AND table_name = 'programming_language' LIMIT 1";
+
+// Database change
+$database_update_sql = "INSERT INTO `programming_language` (`programming_language`) VALUES
+('STOS'),
+('SEUCK'),
+('STAC'),
+('ASM'),
+('GFA Basic'),
+('C');";
+
+// If the update should auto execute without user interaction set to "yes".
+$database_autoexecute = "yes";
+
+// This var should almost allways be set to "no", it is only used for certain corner cases where
+// the database change has already been done in some other way and we only want to update the
+// change database.
+$force_insert = "no";
