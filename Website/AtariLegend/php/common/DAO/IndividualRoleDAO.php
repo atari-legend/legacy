e00a23b9e0fffec78286a93b91bd05d4a2cfeca8
stgraveyard <martens_maarten@hotmail.com>
2018-09-03 00:07:00 +0200

    Game_individual conversion

---
+++ Website/AtariLegend/php/common/DAO/IndividualRoleDAO.php
@@ -0,0 +1,47 @@
+<?php
+namespace AL\Common\DAO;
+
+require_once __DIR__."/../../lib/Db.php" ;
+require_once __DIR__."/../Model/Individual/IndividualRole.php" ;
+
+/**
+ * DAO for Individual Roles
+ */
+class IndividualRoleDAO {
+    private $mysqli;
+
+    public function __construct($mysqli) {
+        $this->mysqli = $mysqli;
+    }
+
+    /**
+     * Get all Roles
+     *
+     * @return \AL\Common\Model\Individual\IndividualRole[] An array of genres
+     */
+    public function getAllIndividualRoles() {
+        $stmt = \AL\Db\execute_query(
+            "IndividualRoleDAO: getAllIndividualRoles",
+            $this->mysqli,
+            "SELECT id, role FROM individual_role ORDER BY role",
+            null, null
+        );
+
+        \AL\Db\bind_result(
+            "IndividualRoleDAO: getAllIndividualRoles",
+            $stmt,
+            $id, $role
+        );
+
+        $individual_roles = [];
+        while ($stmt->fetch()) {
+            $individual_roles[] = new \AL\Common\Model\Individual\IndividualRole(
+                $id, $role
+            );
+        }
+
+        $stmt->close();
+
+        return $individual_roles;
+    }
+}
