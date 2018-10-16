f7a1d4d13fa11ef41c93872be687c0e6bdc7feb3
stgraveyard <martens_maarten@hotmail.com>
2018-10-16 23:07:58 +0200

    Notes fields for copy and disk protection

--- Website/AtariLegend/php/common/DAO/DiskProtectionDAO.php
+++ Website/AtariLegend/php/common/DAO/DiskProtectionDAO.php
@@ -36,7 +36,7 @@ class DiskProtectionDAO {
         $disk_protection_types = [];
         while ($stmt->fetch()) {
             $disk_protection_types[] = new \AL\Common\Model\Game\diskProtection(
-                $id, $name
+                $id, $name, null
             );
         }
 
@@ -54,7 +54,7 @@ class DiskProtectionDAO {
         $stmt = \AL\Db\execute_query(
             "diskProtectionDAO: getDiskProtectionsForRelease",
             $this->mysqli,
-            "SELECT disk_protection_id, name
+            "SELECT disk_protection_id, name, notes
             FROM game_release_disk_protection LEFT JOIN 
             disk_protection ON (game_release_disk_protection.disk_protection_id = disk_protection.id)
             WHERE release_id = ?",
@@ -64,13 +64,13 @@ class DiskProtectionDAO {
         \AL\Db\bind_result(
             "diskProtectionDAO: getDiskProtectionsForRelease",
             $stmt,
-            $disk_protection_id, $name
+            $disk_protection_id, $name, $notes
         );
 
         $disk_protection_types = [];
         while ($stmt->fetch()) {
             $disk_protection_types[] = new \AL\Common\Model\Game\DiskProtection(
-                $disk_protection_id, $name
+                $disk_protection_id, $name, $notes
             );
         }
 
@@ -79,28 +79,39 @@ class DiskProtectionDAO {
         return $disk_protection_types;
     }
     
-    /**
-     * Set the list of disk protection types for this release
+     /**
+     * Add diskProtection for release
      *
-     * @param integer release ID
-     * @param integer[] List of disk protection IDs
+     * @param integer Game Release ID
+     * @param integer protection ID
+     * $param text note
      */
-    public function setDiskProtectionsForRelease($release_id, $disk_protection_id) {
+    public function addDiskProtectionForRelease($game_release_id, $protection_id, $note) {
+       
         $stmt = \AL\Db\execute_query(
-            "diskProtectionDAO: setDiskProtectionsForRelease",
+            "diskProtectionDAO: addDiskProtectionForRelease",
             $this->mysqli,
-            "DELETE FROM game_release_disk_protection WHERE release_id = ?",
-            "i", $release_id
+            "INSERT INTO game_release_disk_protection (release_id, disk_protection_id, notes) VALUES (?, ?, ?)",
+            "iis", $game_release_id, $protection_id, $note
         );
 
-        foreach ($disk_protection_id as $id) {
-            $stmt = \AL\Db\execute_query(
-                "diskProtectionDAO: setDiskProtectionsForRelease",
-                $this->mysqli,
-                "INSERT INTO game_release_disk_protection (release_id, disk_protection_id) VALUES (?, ?)",
-                "ii", $release_id, $id
-            );
-        }
+        $stmt->close();
+    }
+    
+     /**
+     * Delete diskProtection for release
+     *
+     * @param integer Game Release ID
+     * @param integer protection ID
+     */
+    public function deleteDiskProtectionForRelease($game_release_id, $protection_id) {
+        $stmt = \AL\Db\execute_query(
+            "diskProtectionDAO: deleteDiskProtectionForRelease",
+            $this->mysqli,
+            "DELETE FROM game_release_disk_protection
+            WHERE release_id = ? AND disk_protection_id = ?",
+            "ii", $game_release_id, $protection_id
+        );
 
         $stmt->close();
     }
