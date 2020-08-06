14a0c9772f249e708544ece2205fc66c91c0c8c6
Nicolas Guillaumin <nicolas+github@guillaumin.me>
2020-08-06 19:46:03 +0200

    Disable DB upgrade script running
    1and1 restricts SSH :'(

--- deploy.sh
+++ deploy.sh
@@ -38,8 +38,9 @@ if [ -z "${HEAD_BRANCH}" ] && [ "${BRANCH}" == "development" ]; then
     # rsync ${RSYNC_FLAGS[@]} Website/AtariLegend/ $DEV_DEPLOY_USER@$DEV_DEPLOY_HOST:$DEV_DEPLOY_PATH/
     sftp -v -b deploy.sftp $DEV_DEPLOY_USER@$DEV_DEPLOY_HOST
 
+    # 2020.08: Cannot run the script via SSH anymore, 1and1 restricts it
     # Run the DB upgrade script via the PHP command-line interface
-    ssh $DEV_DEPLOY_USER@$DEV_DEPLOY_HOST "cd $DEV_DEPLOY_PATH/php/admin/administration/ && php7.1-cli database_update.php"
+    # ssh $DEV_DEPLOY_USER@$DEV_DEPLOY_HOST "cd $DEV_DEPLOY_PATH/php/admin/administration/ && php7.1-cli database_update.php"
 fi
 
 # Similarly, deploy only on prod when the master branch is built,
@@ -51,6 +52,7 @@ if [ -z "${HEAD_BRANCH}" ] && [ "${BRANCH}" == "master" ]; then
     # rsync ${RSYNC_FLAGS[@]} Website/AtariLegend/ $PROD_DEPLOY_USER@$PROD_DEPLOY_HOST:$PROD_DEPLOY_PATH/
     sftp -v -b deploy.sftp $PROD_DEPLOY_USER@$PROD_DEPLOY_HOST
 
+    # 2020.08: Cannot run the script via SSH anymore, 1and1 restricts it
     # Run the DB upgrade script via the PHP command-line interface
-    ssh $PROD_DEPLOY_USER@$PROD_DEPLOY_HOST "cd $PROD_DEPLOY_PATH/php/admin/administration/ && php7.1-cli database_update.php"
+    # ssh $PROD_DEPLOY_USER@$PROD_DEPLOY_HOST "cd $PROD_DEPLOY_PATH/php/admin/administration/ && php7.1-cli database_update.php"
 fi
